c517fd876926a0a96f1f45630acbf77965092ca4
Daniel Frett <daniel.frett@ccci.org>
2021-05-19 09:02:55 -0400

    add stub for iOS xml parsing

---
+++ src/iosMain/kotlin/org/cru/godtools/tool/xml/IosXmlPullParser.kt
@@ -0,0 +1,5 @@
+package org.cru.godtools.tool.xml
+
+import platform.Foundation.NSXMLParser
+
+class IosXmlPullParser(private val parser: NSXMLParser) : XmlPullParser
--- src/iosTest/kotlin/org/cru/godtools/tool/internal/-IosTestingPlatform.kt
+++ src/iosTest/kotlin/org/cru/godtools/tool/internal/-IosTestingPlatform.kt
@@ -1,11 +1,18 @@
 package org.cru.godtools.tool.internal
 
-import org.cru.godtools.tool.xml.XmlPullParser
+import org.cru.godtools.tool.xml.IosXmlPullParserFactory
 import org.cru.godtools.tool.xml.XmlPullParserFactory
+import platform.Foundation.NSBundle
+import platform.Foundation.NSData
+import platform.Foundation.dataWithContentsOfFile
 
-actual val Any.TEST_XML_PULL_PARSER_FACTORY get() = object : XmlPullParserFactory() {
-    override fun getXmlParser(fileName: String): XmlPullParser? = null
-}
+actual val Any.TEST_XML_PULL_PARSER_FACTORY: XmlPullParserFactory
+    get() = object : IosXmlPullParserFactory() {
+        override fun openFile(fileName: String) = this@TEST_XML_PULL_PARSER_FACTORY::class.qualifiedName
+            ?.replace('.', '/')?.substringBeforeLast('/')
+            ?.let { NSBundle.mainBundle.pathForResource(fileName, null, it) }
+            ?.let { NSData.dataWithContentsOfFile(it) }
+    }
 
 // region Android Robolectric
 actual abstract class Runner
