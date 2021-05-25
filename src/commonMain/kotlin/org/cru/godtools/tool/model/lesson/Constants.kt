8d3914a7c1c7a0d6e7331f3f3436a5fa72fd404f
Daniel Frett <daniel.frett@ccci.org>
2021-05-25 14:36:52 -0400

    mark the DEFAULT_LESSON_CONTROL_COLOR as a SharedImmutable

--- src/commonMain/kotlin/org/cru/godtools/tool/model/lesson/Constants.kt
+++ src/commonMain/kotlin/org/cru/godtools/tool/model/lesson/Constants.kt
@@ -2,10 +2,12 @@ package org.cru.godtools.tool.model.lesson
 
 import org.cru.godtools.tool.internal.AndroidColorInt
 import org.cru.godtools.tool.model.color
+import kotlin.native.concurrent.SharedImmutable
 
 internal const val XMLNS_LESSON = "https://mobile-content-api.cru.org/xmlns/lesson"
 
 internal const val XML_CONTROL_COLOR = "control-color"
 
 @AndroidColorInt
+@SharedImmutable
 internal val DEFAULT_LESSON_CONTROL_COLOR = color(255, 225, 225, 1.0)
