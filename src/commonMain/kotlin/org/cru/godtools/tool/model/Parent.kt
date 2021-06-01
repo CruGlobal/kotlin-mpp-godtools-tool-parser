3aea1d1cf4e0fd754c411e004cf8021d13a69eda
Daniel Frett <daniel.frett@ccci.org>
2021-06-01 14:15:09 -0400

    add Parent interface for elements that contain Content

---
+++ src/commonMain/kotlin/org/cru/godtools/tool/model/Parent.kt
@@ -0,0 +1,30 @@
+package org.cru.godtools.tool.model
+
+import org.cru.godtools.tool.model.Content.Companion.parseContentElement
+import org.cru.godtools.tool.xml.XmlPullParser
+import org.cru.godtools.tool.xml.parseChildren
+
+interface Parent : Base {
+    val content: List<Content>
+}
+
+/**
+ * @param block Custom parsing logic, if the block processes the current tag,
+ * it should advance the parser to the END_TAG event.
+ */
+@OptIn(ExperimentalStdlibApi::class)
+internal inline fun Parent.parseContent(
+    parser: XmlPullParser,
+    block: () -> Unit = { }
+) = buildList {
+    parser.require(XmlPullParser.START_TAG, null, null)
+
+    parser.parseChildren {
+        // execute any custom parsing logic from the call-site
+        // if the block consumes the tag, the parser will be on an END_TAG after returning
+        block()
+        if (parser.eventType == XmlPullParser.END_TAG) return@parseChildren
+
+        parser.parseContentElement(this@parseContent)?.takeUnless { it.isIgnored }?.let { add(it) }
+    }
+}
