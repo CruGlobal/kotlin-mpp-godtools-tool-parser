758cc1471928946a15d43d44ccc66d977128c7ee
Daniel Frett <daniel.frett@ccci.org>
2021-06-01 13:32:58 -0400

    support content restrictions

--- src/commonMain/kotlin/org/cru/godtools/tool/model/Content.kt
+++ src/commonMain/kotlin/org/cru/godtools/tool/model/Content.kt
@@ -1,11 +1,33 @@
 package org.cru.godtools.tool.model
 
 import org.cru.godtools.tool.internal.RestrictTo
+import org.cru.godtools.tool.model.DeviceType.Companion.toDeviceTypes
 import org.cru.godtools.tool.xml.XmlPullParser
 
+private const val XML_RESTRICT_TO = "restrictTo"
+private const val XML_VERSION = "version"
+
 abstract class Content : BaseModel {
-    internal constructor(parent: Base, parser: XmlPullParser) : super(parent)
+    private val version: Int
+    private val restrictTo: Set<DeviceType>
+
+    internal constructor(parent: Base, parser: XmlPullParser) : super(parent) {
+        version = parser.getAttributeValue(null, XML_VERSION)?.toIntOrNull() ?: SCHEMA_VERSION
+        restrictTo = parser.getAttributeValue(XML_RESTRICT_TO)?.toDeviceTypes() ?: DeviceType.ALL
+    }
 
     @RestrictTo(RestrictTo.Scope.TESTS)
-    internal constructor(parent: Base) : super(parent)
+    internal constructor(
+        parent: Base,
+        version: Int = SCHEMA_VERSION,
+        restrictTo: Set<DeviceType> = DeviceType.ALL
+    ) : super(parent) {
+        this.restrictTo = restrictTo
+        this.version = version
+    }
+
+    /**
+     * @return true if this content element should be completely ignored.
+     */
+    open val isIgnored get() = version > SCHEMA_VERSION || restrictTo.none { it in DeviceType.SUPPORTED }
 }
---
+++ src/commonTest/kotlin/org/cru/godtools/tool/model/ContentTest.kt
@@ -0,0 +1,62 @@
+package org.cru.godtools.tool.model
+
+import org.cru.godtools.tool.internal.AndroidJUnit4
+import org.cru.godtools.tool.internal.RunOnAndroidWith
+import kotlin.test.Test
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
+@RunOnAndroidWith(AndroidJUnit4::class)
+class ContentTest {
+    @Test
+    fun verifyRestrictToSupported() {
+        assertFalse(object : Content(Manifest(), restrictTo = DeviceType.ALL) {}.isIgnored)
+        assertFalse(object : Content(Manifest(), restrictTo = DeviceType.SUPPORTED) {}.isIgnored)
+        DeviceType.SUPPORTED.forEach {
+            assertFalse(object : Content(Manifest(), restrictTo = setOf(it)) {}.isIgnored)
+        }
+    }
+
+    @Test
+    fun verifyRestrictToNotSupported() {
+        assertTrue(object : Content(Manifest(), restrictTo = setOf(DeviceType.UNKNOWN)) {}.isIgnored)
+        assertTrue(object : Content(Manifest(), restrictTo = DeviceType.ALL - DeviceType.SUPPORTED) {}.isIgnored)
+        (DeviceType.ALL - DeviceType.SUPPORTED).forEach {
+            assertTrue(object : Content(Manifest(), restrictTo = setOf(it)) {}.isIgnored)
+        }
+    }
+
+    @Test
+    fun verifyVersionSupported() {
+        assertFalse(object : Content(Manifest(), version = SCHEMA_VERSION) {}.isIgnored)
+    }
+
+    @Test
+    fun verifyVersionNotSupported() {
+        assertTrue(object : Content(Manifest(), version = SCHEMA_VERSION + 1) {}.isIgnored)
+    }
+//
+//    @Test
+//    fun verifyFromXmlParagraph() {
+//        val content = Content.fromXml(Manifest(), getXmlParserForResource("paragraph.xml"), true)
+//        assertTrue(content is Paragraph)
+//    }
+//
+//    @Test
+//    fun verifyFromXmlParagraphFallback() {
+//        val content = Content.fromXml(Manifest(), getXmlParserForResource("fallback_paragraph.xml"), true)
+//        assertTrue(content is Fallback)
+//    }
+//
+//    @Test
+//    fun verifyFromXmlFallback() {
+//        val content = Content.fromXml(Manifest(), getXmlParserForResource("fallback.xml"), true)
+//        assertTrue(content is Fallback)
+//    }
+//
+//    @Test
+//    fun testFromXmlSpacer() {
+//        val content = Content.fromXml(Manifest(), getXmlParserForResource("spacer.xml"), true)
+//        assertTrue(content is Spacer)
+//    }
+}
