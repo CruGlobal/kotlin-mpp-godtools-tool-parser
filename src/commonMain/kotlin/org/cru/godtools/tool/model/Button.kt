2f66e401ac2ce2a90e52d23ebcf95cd9c342069a
Daniel Frett <daniel.frett@ccci.org>
2021-06-07 12:16:13 -0400

    support parsing most of the button attributes

--- src/commonMain/kotlin/org/cru/godtools/tool/model/Button.kt
+++ src/commonMain/kotlin/org/cru/godtools/tool/model/Button.kt
@@ -1,15 +1,82 @@
 package org.cru.godtools.tool.model
 
+import org.cru.godtools.tool.internal.AndroidColorInt
+import org.cru.godtools.tool.internal.RestrictTo
+import org.cru.godtools.tool.model.AnalyticsEvent.Companion.parseAnalyticsEvents
+import org.cru.godtools.tool.model.Button.Style.Companion.toButtonStyle
+import org.cru.godtools.tool.model.Button.Type.Companion.toButtonTypeOrNull
 import org.cru.godtools.tool.xml.XmlPullParser
 
+private const val XML_COLOR = "color"
+private const val XML_TYPE = "type"
 private const val XML_TYPE_EVENT = "event"
 private const val XML_TYPE_URL = "url"
+private const val XML_STYLE = "style"
 private const val XML_STYLE_CONTAINED = "contained"
 private const val XML_STYLE_OUTLINED = "outlined"
 
 class Button : Content, Styles {
-    internal constructor(parent: Base, parser: XmlPullParser) : super(parent, parser) {
+    companion object {
+        internal const val XML_BUTTON = "button"
     }
+
+    val type: Type
+    val events: List<EventId>
+
+    private val _style: Style?
+    val style: Style get() = _style ?: stylesParent.buttonStyle
+
+    @AndroidColorInt
+    private val _buttonColor: Color?
+    @get:AndroidColorInt
+    override val buttonColor get() = _buttonColor ?: stylesParent.let { it?.buttonColor ?: it.primaryColor }
+
+    val text: Text?
+    override val textAlign get() = Text.Align.CENTER
+    override val textColor get() = stylesParent.primaryTextColor
+
+    val analyticsEvents: Collection<AnalyticsEvent>
+
+    internal constructor(parent: Base, parser: XmlPullParser) : super(parent, parser) {
+        parser.require(XmlPullParser.START_TAG, XMLNS_CONTENT, XML_BUTTON)
+
+        type = parser.getAttributeValue(XML_TYPE)?.toButtonTypeOrNull() ?: Type.DEFAULT
+        events = parser.getAttributeValue(XML_EVENTS).toEventIds()
+
+        _style = parser.getAttributeValue(XML_STYLE)?.toButtonStyle()
+        _buttonColor = parser.getAttributeValue(XML_COLOR)?.toColorOrNull()
+
+        // process any child elements
+        val analyticsEvents = mutableListOf<AnalyticsEvent>()
+        text = parser.parseTextChild(this, XMLNS_CONTENT, XML_BUTTON) {
+            when (parser.namespace) {
+                XMLNS_ANALYTICS -> when (parser.name) {
+                    AnalyticsEvent.XML_EVENTS -> analyticsEvents += parser.parseAnalyticsEvents(this)
+                }
+            }
+        }
+        this.analyticsEvents = analyticsEvents
+    }
+
+    @RestrictTo(RestrictTo.Scope.TESTS)
+    internal constructor(
+        parent: Base,
+        type: Type = Type.DEFAULT,
+        style: Style? = null,
+        @AndroidColorInt color: Color? = null,
+        text: ((Button) -> Text?)? = null
+    ) : super(parent) {
+        this.type = type
+        events = emptyList()
+
+        _style = style
+        _buttonColor = color
+
+        analyticsEvents = emptySet()
+        this.text = text?.invoke(this)
+    }
+
+    override val isIgnored get() = super.isIgnored || type == Type.UNKNOWN || style == Style.UNKNOWN
 
     enum class Type {
         EVENT, URL, UNKNOWN;
@@ -37,3 +104,6 @@ class Button : Content, Styles {
         }
     }
 }
+
+val Button?.buttonColor get() = this?.buttonColor ?: stylesParent.primaryColor
+val Button?.textColor get() = this?.textColor ?: stylesParent.primaryTextColor
--- src/commonTest/kotlin/org/cru/godtools/tool/model/ButtonTest.kt
+++ src/commonTest/kotlin/org/cru/godtools/tool/model/ButtonTest.kt
@@ -1,12 +1,99 @@
 package org.cru.godtools.tool.model
 
+import org.cru.godtools.tool.ParserConfig
+import org.cru.godtools.tool.internal.AndroidJUnit4
+import org.cru.godtools.tool.internal.RunOnAndroidWith
+import org.cru.godtools.tool.internal.UsesResources
 import org.cru.godtools.tool.model.Button.Style.Companion.toButtonStyle
 import org.cru.godtools.tool.model.Button.Type.Companion.toButtonTypeOrNull
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertNotEquals
 import kotlin.test.assertNull
+import kotlin.test.assertTrue
 
-class ButtonTest {
+@RunOnAndroidWith(AndroidJUnit4::class)
+class ButtonTest : UsesResources() {
+    private val parent = object : BaseModel(), Styles {
+        override lateinit var buttonStyle: Button.Style
+        override var primaryColor = TestColors.BLACK
+        override var primaryTextColor = TestColors.BLACK
+    }
+
+    @Test
+    fun testParseButtonEvent() {
+        val manifest = Manifest()
+        val button = Button(manifest, getTestXmlParser("button_event.xml"))
+        assertFalse(button.isIgnored)
+        assertEquals(manifest.buttonStyle, button.style)
+        assertEquals(Button.Type.EVENT, button.type)
+        assertEquals(EventId.parse("event1 event2"), button.events)
+        assertEquals("event button", button.text!!.text)
+        assertEquals(TestColors.RED, button.buttonColor)
+    }
+
+    @Test
+    fun testParseButtonRestrictTo() {
+        val button = Button(Manifest(), getTestXmlParser("button_restrictTo.xml"))
+        ParserConfig.supportedDeviceTypes = setOf(DeviceType.WEB)
+        assertFalse(button.isIgnored)
+        ParserConfig.supportedDeviceTypes = setOf(DeviceType.MOBILE)
+        assertTrue(button.isIgnored)
+    }
+
+    @Test
+    fun testButtonTypeUnknown() {
+        val button = Button(Manifest(), type = Button.Type.UNKNOWN)
+        assertTrue(button.isIgnored)
+    }
+
+    @Test
+    fun testButtonStyleUnknown() {
+        val button = Button(Manifest(), style = Button.Style.UNKNOWN)
+        assertTrue(button.isIgnored)
+    }
+
+    @Test
+    fun testButtonStyleUtilizesStylesDefault() {
+        val button = Button(parent)
+        parent.buttonStyle = Button.Style.CONTAINED
+        assertEquals(Button.Style.CONTAINED, button.buttonStyle)
+        parent.buttonStyle = Button.Style.OUTLINED
+        assertEquals(Button.Style.OUTLINED, button.buttonStyle)
+    }
+
+    @Test
+    fun testButtonColorFallbackBehavior() {
+        val manifest = Manifest()
+        assertEquals(manifest.primaryColor, Button(manifest).buttonColor)
+        with(Button(manifest, color = TestColors.GREEN)) {
+            assertEquals(TestColors.GREEN, buttonColor)
+            assertNotEquals(manifest.primaryColor, buttonColor)
+        }
+
+        val parent = object : BaseModel(manifest), Styles {
+            override val primaryColor = TestColors.BLUE
+        }
+        with(Button(parent)) {
+            assertEquals(parent.primaryColor, buttonColor)
+            assertNotEquals(manifest.primaryColor, buttonColor)
+        }
+        with(Button(parent, color = TestColors.GREEN)) {
+            assertEquals(TestColors.GREEN, buttonColor)
+            assertNotEquals(parent.primaryColor, buttonColor)
+            assertNotEquals(manifest.primaryColor, buttonColor)
+        }
+    }
+
+    @Test
+    fun testButtonTextColorFallbackBehavior() {
+        parent.primaryColor = TestColors.RED
+        parent.primaryTextColor = TestColors.GREEN
+        assertEquals(TestColors.BLUE, Button(parent, text = { Text(it, textColor = TestColors.BLUE) }).text!!.textColor)
+        assertEquals(TestColors.GREEN, Button(parent, text = { Text(it, textColor = null) }).text!!.textColor)
+    }
+
     // region Button.Style
     @Test
     fun verifyParseButtonStyle() {
