e3ac89a2bc16251558841b487e733d324af049a6
Daniel Frett <daniel.frett@ccci.org>
2021-06-14 15:41:31 -0400

    add a couple extension properties to the Uri object

--- src/jsMain/kotlin/org/cru/godtools/tool/model/JsUri.kt
+++ src/jsMain/kotlin/org/cru/godtools/tool/model/JsUri.kt
@@ -1,4 +1,6 @@
 package org.cru.godtools.tool.model
 
 actual typealias Uri = String
+internal actual inline val Uri.scheme: String? get() = if (contains(":")) substringBefore(":") else null
+
 internal actual fun String.toUri() = this
