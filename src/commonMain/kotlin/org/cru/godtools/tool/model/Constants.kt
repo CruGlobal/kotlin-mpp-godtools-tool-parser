7df180a0ab423efb596dba04bbcb6eef3ff84f68
Daniel Frett <daniel.frett@ccci.org>
2021-06-16 14:10:53 -0400

    use textScale to represent the relative size of Header text children

--- src/commonMain/kotlin/org/cru/godtools/tool/model/Constants.kt
+++ src/commonMain/kotlin/org/cru/godtools/tool/model/Constants.kt
@@ -32,6 +32,8 @@ internal val WHITE = color(255, 255, 255, 1.0)
 // region Text Sizes
 // these text sizes are exposed publicly via relative textScale properties based off the base text size
 internal const val TEXT_SIZE_BASE = 16
+internal const val TEXT_SIZE_HEADER = 18
+internal const val TEXT_SIZE_HEADER_NUMBER = 54
 internal const val TEXT_SIZE_MODAL = 18
 internal const val TEXT_SIZE_MODAL_TITLE = 54
 // endregion Text Sizes
