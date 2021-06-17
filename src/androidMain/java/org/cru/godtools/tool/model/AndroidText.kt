b35958e9b6d1a52e78b8bb09647d0635e9ae3f26
Daniel Frett <daniel.frett@ccci.org>
2021-06-17 16:12:29 -0400

    provide a typefaceStyle property on Text nodes for Android

--- src/androidMain/java/org/cru/godtools/tool/model/AndroidText.kt
+++ src/androidMain/java/org/cru/godtools/tool/model/AndroidText.kt
@@ -1,6 +1,13 @@
 package org.cru.godtools.tool.model
 
+import android.graphics.Typeface
 import android.view.Gravity
+import splitties.bitflags.withFlag
+
+val Text.typefaceStyle
+    get() = Typeface.NORMAL
+        .withFlag(if (Text.Style.BOLD in textStyles) Typeface.BOLD else 0)
+        .withFlag(if (Text.Style.ITALIC in textStyles) Typeface.ITALIC else 0)
 
 val Text.Align.gravity get() = when (this) {
     Text.Align.START -> Gravity.START
--- src/androidTest/java/org/cru/godtools/tool/model/AndroidTextTest.kt
+++ src/androidTest/java/org/cru/godtools/tool/model/AndroidTextTest.kt
@@ -1,5 +1,6 @@
 package org.cru.godtools.tool.model
 
+import android.graphics.Typeface
 import android.view.Gravity
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Test
@@ -13,5 +14,16 @@ class AndroidTextTest {
         assertEquals(Gravity.START, Text.Align.START.gravity)
         assertEquals(Gravity.CENTER_HORIZONTAL, Text.Align.CENTER.gravity)
         assertEquals(Gravity.END, Text.Align.END.gravity)
+    }
+
+    @Test
+    fun testTypefaceStyle() {
+        assertEquals(Typeface.NORMAL, Text(Manifest()).typefaceStyle)
+        assertEquals(Typeface.BOLD, Text(Manifest(), textStyles = setOf(Text.Style.BOLD)).typefaceStyle)
+        assertEquals(Typeface.ITALIC, Text(Manifest(), textStyles = setOf(Text.Style.ITALIC)).typefaceStyle)
+        assertEquals(
+            Typeface.BOLD_ITALIC,
+            Text(Manifest(), textStyles = setOf(Text.Style.BOLD, Text.Style.ITALIC)).typefaceStyle
+        )
     }
 }
