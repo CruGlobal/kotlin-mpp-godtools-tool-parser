49e5722e75a3320ab16d6a0013f67fd2ef6391fc
Daniel Frett <daniel.frett@ccci.org>
2021-06-21 13:46:32 -0400

    add a CallToAction test constructor

--- src/commonMain/kotlin/org/cru/godtools/tool/model/tract/CallToAction.kt
+++ src/commonMain/kotlin/org/cru/godtools/tool/model/tract/CallToAction.kt
@@ -1,6 +1,7 @@
 package org.cru.godtools.tool.model.tract
 
 import org.cru.godtools.tool.internal.AndroidColorInt
+import org.cru.godtools.tool.internal.RestrictTo
 import org.cru.godtools.tool.internal.VisibleForTesting
 import org.cru.godtools.tool.model.BaseModel
 import org.cru.godtools.tool.model.Color
@@ -45,15 +46,30 @@ class CallToAction : BaseModel {
         tipId = null
     }
 
-    internal constructor(parent: TractPage, parser: XmlPullParser) : super(parent) {
+    internal constructor(page: TractPage, parser: XmlPullParser) : super(page) {
         parser.require(XmlPullParser.START_TAG, XMLNS_TRACT, XML_CALL_TO_ACTION)
 
-        page = parent
+        this.page = page
         events = parser.getAttributeValue(XML_EVENTS).toEventIds()
         _controlColor = parser.getAttributeValue(XML_CONTROL_COLOR)?.toColorOrNull()
         tipId = parser.getAttributeValue(XMLNS_TRAINING, XML_TIP)
 
         label = parser.parseTextChild(this, XMLNS_TRACT, XML_CALL_TO_ACTION)
+    }
+
+    @RestrictTo(RestrictTo.Scope.TESTS)
+    constructor(
+        page: TractPage = TractPage(),
+        label: ((CallToAction) -> Text)? = null,
+        events: List<EventId> = emptyList(),
+        @AndroidColorInt controlColor: Color? = null,
+        tip: String? = null
+    ) : super(page) {
+        this.page = page
+        this.label = label?.invoke(this)
+        this.events = events
+        _controlColor = controlColor
+        tipId = tip
     }
 }
 
