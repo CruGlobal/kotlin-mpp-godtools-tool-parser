9019219fdf5926f0cb5ca7c1b2dac1df955be8e7
Daniel Frett <daniel.frett@ccci.org>
2021-06-22 16:15:26 -0400

    Revert "use textScale to represent the relative size of Header text children"
    This reverts commit 7df180a0ab423efb596dba04bbcb6eef3ff84f68.

--- src/commonTest/kotlin/org/cru/godtools/tool/model/tract/HeaderTest.kt
+++ src/commonTest/kotlin/org/cru/godtools/tool/model/tract/HeaderTest.kt
@@ -4,11 +4,7 @@ import org.cru.godtools.tool.internal.AndroidJUnit4
 import org.cru.godtools.tool.internal.RunOnAndroidWith
 import org.cru.godtools.tool.internal.UsesResources
 import org.cru.godtools.tool.model.Manifest
-import org.cru.godtools.tool.model.TEXT_SIZE_BASE
-import org.cru.godtools.tool.model.TEXT_SIZE_HEADER
-import org.cru.godtools.tool.model.TEXT_SIZE_HEADER_NUMBER
 import org.cru.godtools.tool.model.TestColors
-import org.cru.godtools.tool.model.Text
 import org.cru.godtools.tool.model.primaryColor
 import org.cru.godtools.tool.model.stylesParent
 import org.cru.godtools.tool.model.tips.Tip
@@ -47,24 +43,6 @@ class HeaderTest : UsesResources("model/tract") {
         with(null as Header?) { assertEquals(stylesParent.primaryColor, backgroundColor) }
         with(header as Header?) { assertEquals(TestColors.GREEN, backgroundColor) }
         with(header) { assertEquals(TestColors.GREEN, backgroundColor) }
-    }
-
-    @Test
-    fun testTextScale() {
-        with(Header(number = { Text(it) }, title = { Text(it) })) {
-            assertEquals(TEXT_SIZE_HEADER, (TEXT_SIZE_BASE * title!!.textScale).toInt())
-            assertEquals(TEXT_SIZE_HEADER_NUMBER, (TEXT_SIZE_BASE * number!!.textScale).toInt())
-        }
-
-        with(Header(TractPage(textScale = 2.0), number = { Text(it) }, title = { Text(it) })) {
-            assertEquals(2 * TEXT_SIZE_HEADER, (TEXT_SIZE_BASE * title!!.textScale).toInt())
-            assertEquals(2 * TEXT_SIZE_HEADER_NUMBER, (TEXT_SIZE_BASE * number!!.textScale).toInt())
-        }
-
-        with(Header(number = { Text(it, textScale = 3.0) }, title = { Text(it, textScale = 2.0) })) {
-            assertEquals(2 * TEXT_SIZE_HEADER, (TEXT_SIZE_BASE * title!!.textScale).toInt())
-            assertEquals(3 * TEXT_SIZE_HEADER_NUMBER, (TEXT_SIZE_BASE * number!!.textScale).toInt())
-        }
     }
 
     @Test
