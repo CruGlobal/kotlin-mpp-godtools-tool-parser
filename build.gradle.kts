468d40007dd896365d22f7a5c9df0de5d3a4162a
Daniel Frett <daniel.frett@ccci.org>
2021-06-24 09:49:03 -0400

    the iOS workaround is still necessary

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,8 +35,8 @@ kotlin {
     }
     // HACK: workaround https://youtrack.jetbrains.com/issue/KT-40975
     //       See also: https://kotlinlang.org/docs/mobile/add-dependencies.html#workaround-to-enable-ide-support-for-the-shared-ios-source-set
-    //       This should be able to go away when we upgrade to Kotlin 1.5.20
-//    ios()
+    //       This should be able to go away when we upgrade to Kotlin 1.5.30
+//    ios { copyTestResources() }
     when {
         System.getenv("SDK_NAME")?.startsWith("iphoneos") == true -> iosArm64("ios")
         else -> iosX64("ios")
