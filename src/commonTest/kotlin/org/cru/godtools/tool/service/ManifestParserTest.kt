aadec140d306cc65315054a78b56f215c23d1fcd
Daniel Frett <daniel.frett@ccci.org>
2021-06-28 10:23:41 -0400

    make parseManifest suspendable

--- src/commonTest/kotlin/org/cru/godtools/tool/service/ManifestParserTest.kt
+++ src/commonTest/kotlin/org/cru/godtools/tool/service/ManifestParserTest.kt
@@ -4,6 +4,7 @@ import org.cru.godtools.tool.internal.AndroidJUnit4
 import org.cru.godtools.tool.internal.RunOnAndroidWith
 import org.cru.godtools.tool.internal.TEST_XML_PULL_PARSER_FACTORY
 import org.cru.godtools.tool.internal.UsesResources
+import org.cru.godtools.tool.internal.runBlockingTest
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertIs
@@ -13,28 +14,28 @@ class ManifestParserTest : UsesResources("service") {
     private val parser = ManifestParser(TEST_XML_PULL_PARSER_FACTORY)
 
     @Test
-    fun testParseManifest() {
+    fun testParseManifest() = runBlockingTest {
         val result = assertIs<Result.Data>(parser.parseManifest("manifest_valid.xml"))
         assertEquals(2, result.manifest.tractPages.size)
     }
 
     @Test
-    fun testParseManifestMissingManifest() {
+    fun testParseManifestMissingManifest() = runBlockingTest {
         assertIs<Result.Error.NotFound>(parser.parseManifest("missing.xml"))
     }
 
     @Test
-    fun testParseManifestInvalidManifest() {
+    fun testParseManifestInvalidManifest() = runBlockingTest {
         assertIs<Result.Error.Corrupted>(parser.parseManifest("../model/accordion.xml"))
     }
 
     @Test
-    fun testParseManifestMissingPage() {
+    fun testParseManifestMissingPage() = runBlockingTest {
         assertIs<Result.Error.NotFound>(parser.parseManifest("manifest_missing_page.xml"))
     }
 
     @Test
-    fun testParseManifestInvalidPage() {
+    fun testParseManifestInvalidPage() = runBlockingTest {
         assertIs<Result.Error.Corrupted>(parser.parseManifest("manifest_invalid_page.xml"))
     }
 }
