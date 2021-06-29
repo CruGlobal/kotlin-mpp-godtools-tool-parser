82f544e6f94c6d9ce3787ec11c67b5894d54923a
Daniel Frett <daniel.frett@ccci.org>
2021-06-29 16:14:23 -0400

    make state methods internal
    I'm aiming to make the State object opaque to apps using it, it's just an object that is passed into various model objects that interact with the state

--- src/commonMain/kotlin/org/cru/godtools/tool/state/State.kt
+++ src/commonMain/kotlin/org/cru/godtools/tool/state/State.kt
@@ -9,12 +9,12 @@ import org.cru.godtools.tool.model.EventId
 class State internal constructor(private val state: MutableMap<String, List<String>>) : Parcelable {
     constructor() : this(mutableMapOf<String, List<String>>())
 
-    operator fun get(key: String) = state[key]?.firstOrNull()
-    fun getAll(key: String) = state[key].orEmpty()
+    internal operator fun get(key: String) = state[key]?.firstOrNull()
+    internal fun getAll(key: String) = state[key].orEmpty()
 
-    operator fun set(key: String, value: String?) = state.set(key, listOfNotNull(value))
-    operator fun set(key: String, values: List<String>?) = state.set(key, values.orEmpty())
-    fun set(key: String, vararg values: String) = state.set(key, values.toList())
+    internal operator fun set(key: String, value: String?) = state.set(key, listOfNotNull(value))
+    internal operator fun set(key: String, values: List<String>?) = state.set(key, values.orEmpty())
+    internal fun set(key: String, vararg values: String) = state.set(key, values.toList())
 
     fun resolveEventId(id: EventId) = when (id.namespace) {
         EVENT_NAMESPACE_STATE -> getAll(id.name).map { EventId(name = it) }
