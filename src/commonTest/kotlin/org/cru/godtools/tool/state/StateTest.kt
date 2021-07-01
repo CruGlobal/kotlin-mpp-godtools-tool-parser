2cb64fcf7f811c81d2f896f036016903555ad97f
Daniel Frett <daniel.frett@ccci.org>
2021-07-01 11:53:16 -0400

    add a changeFlow method to State
    this will allow other code to monitor State for changes

--- src/commonTest/kotlin/org/cru/godtools/tool/state/StateTest.kt
+++ src/commonTest/kotlin/org/cru/godtools/tool/state/StateTest.kt
@@ -1,5 +1,11 @@
 package org.cru.godtools.tool.state
 
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import org.cru.godtools.tool.internal.receive
+import org.cru.godtools.tool.internal.runBlockingTest
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertNull
@@ -24,5 +30,28 @@ class StateTest {
         assertEquals(listOf("value"), state.getAll("single"))
         state["multiple"] = listOf("a", "b", "c")
         assertEquals(listOf("a", "b", "c"), state.getAll("multiple"))
+    }
+
+    @Test
+    fun testChangeFlow() = runBlockingTest {
+        val channel = Channel<String>()
+        val flow = state.changeFlow("test")
+            .onEach { channel.send(it) }
+            .launchIn(this)
+
+        // initial value
+        assertEquals("test", channel.receive(100))
+
+        // update state for monitored key
+        state["test"] = "a"
+        assertEquals("test", channel.receive(100))
+
+        // update state for a different key
+        state["other"] = "a"
+        delay(100)
+        assertTrue(channel.isEmpty)
+
+        // shut down flow
+        flow.cancel()
     }
 }
