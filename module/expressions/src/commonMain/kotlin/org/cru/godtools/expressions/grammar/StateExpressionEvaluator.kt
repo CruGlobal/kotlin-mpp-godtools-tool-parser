89f0e56e0f409e118583a418847530a7c3ecdce9
Daniel Frett <daniel.frett@ccci.org>
2021-08-05 16:51:45 -0400

    make it possible to evaluate both boolean and int expressions with StateExpressionEvaluator

--- module/expressions/src/commonMain/kotlin/org/cru/godtools/expressions/grammar/StateExpressionEvaluator.kt
+++ module/expressions/src/commonMain/kotlin/org/cru/godtools/expressions/grammar/StateExpressionEvaluator.kt
@@ -5,30 +5,34 @@ import org.cru.godtools.expressions.grammar.generated.StateExpressionParser
 import org.cru.godtools.expressions.grammar.generated.StateExpressionParser.Tokens
 import org.cru.godtools.tool.state.State
 
-internal class StateExpressionEvaluator(private val state: State) : StateExpressionBaseVisitor<Boolean>() {
-    override fun visitParExpr(ctx: StateExpressionParser.ParExprContext) = ctx.expr!!.accept(this)
-    override fun visitNotExpr(ctx: StateExpressionParser.NotExprContext) = !ctx.expr!!.accept(this)
-    override fun visitOrExpr(ctx: StateExpressionParser.OrExprContext) =
-        ctx.left!!.accept(this) || ctx.right!!.accept(this)
-    override fun visitAndExpr(ctx: StateExpressionParser.AndExprContext) =
-        ctx.left!!.accept(this) && ctx.right!!.accept(this)
+internal class StateExpressionEvaluator(private val state: State) {
+    val booleanExpr = object : StateExpressionBaseVisitor<Boolean>() {
+        override fun visitParExpr(ctx: StateExpressionParser.ParExprContext) = ctx.expr!!.accept(this)
+        override fun visitNotExpr(ctx: StateExpressionParser.NotExprContext) = !ctx.expr!!.accept(this)
+        override fun visitOrExpr(ctx: StateExpressionParser.OrExprContext) =
+            ctx.left!!.accept(this) || ctx.right!!.accept(this)
 
-    override fun visitBooleanAtom(ctx: StateExpressionParser.BooleanAtomContext) = when (ctx.atom!!.type) {
-        Tokens.TRUE.id -> true
-        Tokens.FALSE.id -> false
-        else -> throw IllegalStateException()
-    }
+        override fun visitAndExpr(ctx: StateExpressionParser.AndExprContext) =
+            ctx.left!!.accept(this) && ctx.right!!.accept(this)
 
-    override fun visitEqExpr(ctx: StateExpressionParser.EqExprContext): Boolean {
-        val varName = ctx.varName!!.text!!
-        val value = ctx.value!!.text!!.run { substring(1, length - 1) }
-        return when (ctx.op!!.type) {
-            Tokens.EQ.id -> state.getAll(varName).contains(value)
-            Tokens.NEQ.id -> !state.getAll(varName).contains(value)
+        override fun visitBooleanAtom(ctx: StateExpressionParser.BooleanAtomContext) = when (ctx.atom!!.type) {
+            Tokens.TRUE.id -> true
+            Tokens.FALSE.id -> false
             else -> throw IllegalStateException()
         }
-    }
 
-    override fun visitIsSetFunc(ctx: StateExpressionParser.IsSetFuncContext) =
-        !state.getAll(ctx.varName!!.text!!).isEmpty()
+        override fun visitEqExpr(ctx: StateExpressionParser.EqExprContext): Boolean {
+            val varName = ctx.varName!!.text!!
+            val value = ctx.value!!.text!!.run { substring(1, length - 1) }
+            return when (ctx.op!!.type) {
+                Tokens.EQ.id -> state.getAll(varName).contains(value)
+                Tokens.NEQ.id -> !state.getAll(varName).contains(value)
+                else -> throw IllegalStateException()
+            }
+        }
+
+        override fun visitIsSetFunc(ctx: StateExpressionParser.IsSetFuncContext) =
+            state.getAll(ctx.varName!!.text!!).isNotEmpty()
+    }
+    val intExpr = object : StateExpressionBaseVisitor<Int>() {}
 }
