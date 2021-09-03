6c550d7c8c1ad74c4c21f30216c95c10fe804377
Daniel Frett <daniel.frett@ccci.org>
2021-09-03 13:56:40 -0400

    update Button to use the clicked trigger type, and log a warning about selected trigger types

--- module/parser/src/commonTest/kotlin/org/cru/godtools/tool/model/ButtonTest.kt
+++ module/parser/src/commonTest/kotlin/org/cru/godtools/tool/model/ButtonTest.kt
@@ -128,12 +128,14 @@ class ButtonTest : UsesResources() {
     @Test
     fun testButtonGetAnalyticsEvents() {
         val defaultEvent = AnalyticsEvent(trigger = Trigger.DEFAULT)
+        val clickedEvent = AnalyticsEvent(trigger = Trigger.CLICKED)
         val selectedEvent = AnalyticsEvent(trigger = Trigger.SELECTED)
         val visibleEvent = AnalyticsEvent(trigger = Trigger.VISIBLE)
-        val button = Button(analyticsEvents = listOf(defaultEvent, selectedEvent, visibleEvent))
+        val button = Button(analyticsEvents = listOf(defaultEvent, clickedEvent, selectedEvent, visibleEvent))
 
-        assertEquals(listOf(defaultEvent, selectedEvent), button.getAnalyticsEvents(Trigger.SELECTED))
+        assertEquals(listOf(defaultEvent, clickedEvent, selectedEvent), button.getAnalyticsEvents(Trigger.CLICKED))
         assertFailsWith(IllegalStateException::class) { button.getAnalyticsEvents(Trigger.DEFAULT) }
+        assertFailsWith(IllegalStateException::class) { button.getAnalyticsEvents(Trigger.SELECTED) }
         assertFailsWith(IllegalStateException::class) { button.getAnalyticsEvents(Trigger.VISIBLE) }
     }
 
