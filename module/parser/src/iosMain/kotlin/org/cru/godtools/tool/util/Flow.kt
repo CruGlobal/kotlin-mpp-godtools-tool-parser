0618ebc324ed3681aab7722dea53a131c2767183
Daniel Frett <daniel.frett@ccci.org>
2021-09-09 16:55:57 -0400

    add an iOS unit test for watching a flow

--- module/parser/src/iosMain/kotlin/org/cru/godtools/tool/util/Flow.kt
+++ module/parser/src/iosMain/kotlin/org/cru/godtools/tool/util/Flow.kt
@@ -6,13 +6,15 @@ import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
+import kotlin.coroutines.CoroutineContext
 
-internal fun <T> Flow<T>.watch(block: (T) -> Unit): FlowWatcher {
+internal fun <T> Flow<T>.watch(block: (T) -> Unit) = watch(Dispatchers.Main, block)
+internal fun <T> Flow<T>.watch(context: CoroutineContext, block: (T) -> Unit): FlowWatcher {
     val job = Job()
 
     onEach {
         block(it)
-    }.launchIn(CoroutineScope(Dispatchers.Main + job))
+    }.launchIn(CoroutineScope(context + job))
 
     return FlowWatcher(job)
 }
