a440cfc4c7f1898ae5f3d3309369ce35a40641a3
Daniel Frett <daniel.frett@ccci.org>
2021-09-13 13:59:33 -0400

    update logic that parses/creates colors

--- module/parser/src/commonMain/kotlin/org/cru/godtools/tool/model/PlatformColor.kt
+++ module/parser/src/commonMain/kotlin/org/cru/godtools/tool/model/PlatformColor.kt
@@ -8,14 +8,21 @@ import kotlin.native.concurrent.SharedImmutable
 @SharedImmutable
 private val COLOR_REGEX =
     Regex("^\\s*rgba\\(\\s*([0-9]+)\\s*,\\s*([0-9]+)\\s*,\\s*([0-9]+)\\s*,\\s*([0-9.]+)\\s*\\)\\s*$")
+@SharedImmutable
+private val VALID_RGB = 0..255
+@SharedImmutable
+private val VALID_ALPHA = 0f..1f
 
 expect class PlatformColor
 
 @AndroidColorInt
 internal fun String.toColorOrNull(): PlatformColor? = COLOR_REGEX.matchEntire(this)?.let {
     try {
-        val (red, green, blue, alpha) = it.destructured
-        RGB(red.toInt(), green.toInt(), blue.toInt(), alpha.toFloat()).toPlatformColor()
+        val red = it.groupValues[1].toInt().also { require(it in VALID_RGB) }
+        val green = it.groupValues[2].toInt().also { require(it in VALID_RGB) }
+        val blue = it.groupValues[3].toInt().also { require(it in VALID_RGB) }
+        val alpha = it.groupValues[4].toDouble().also { require(it in VALID_ALPHA) }
+        color(red, green, blue, alpha)
     } catch (ignored: IllegalArgumentException) {
         null
     }
@@ -23,7 +30,7 @@ internal fun String.toColorOrNull(): PlatformColor? = COLOR_REGEX.matchEntire(th
 
 @AndroidColorInt
 internal inline fun color(red: Int, green: Int, blue: Int, alpha: Double) =
-    RGB(red, green, blue, alpha.toFloat()).toPlatformColor()
+    RGB(red / 255f, green / 255f, blue / 255f, alpha).toPlatformColor()
 
 internal expect fun PlatformColor.toRGB(): RGB
 internal expect fun RGB.toPlatformColor(): PlatformColor
