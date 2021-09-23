86c1504ea779aaf68668b9900b978bb8b934eb0f
Daniel Frett <daniel.frett@ccci.org>
2021-09-23 16:22:25 -0400

    create a new Clickable interface

---
+++ module/parser/src/commonMain/kotlin/org/cru/godtools/tool/model/Clickable.kt
@@ -0,0 +1,23 @@
+package org.cru.godtools.tool.model
+
+import org.cru.godtools.tool.xml.XmlPullParser
+import kotlin.contracts.ExperimentalContracts
+import kotlin.contracts.InvocationKind
+import kotlin.contracts.contract
+
+private const val XML_URL = "url"
+
+interface Clickable : Base {
+    val isClickable get() = url != null || events.isNotEmpty()
+
+    val events: List<EventId>
+    val url: Uri?
+}
+
+@OptIn(ExperimentalContracts::class)
+internal inline fun XmlPullParser.parseClickableAttrs(block: (List<EventId>, Uri?) -> Unit) {
+    contract {
+        callsInPlace(block, InvocationKind.EXACTLY_ONCE)
+    }
+    block(getAttributeValue(XML_EVENTS).toEventIds(), getAttributeValue(XML_URL)?.toAbsoluteUri())
+}
