22142387d85dcf43c3797cfd1303288f213d8295
Daniel Frett <daniel.frett@ccci.org>
2021-09-27 09:59:48 -0400

    utilize new colorint extension module from colormath

--- module/parser/src/androidMain/java/org/cru/godtools/tool/model/AndroidColor.kt
+++ module/parser/src/androidMain/java/org/cru/godtools/tool/model/AndroidColor.kt
@@ -1,12 +1,12 @@
 package org.cru.godtools.tool.model
 
-import android.graphics.Color
 import androidx.annotation.ColorInt
+import com.github.ajalt.colormath.extensions.android.colorint.fromColorInt
+import com.github.ajalt.colormath.extensions.android.colorint.toColorInt
 import com.github.ajalt.colormath.model.RGB
 
 actual typealias PlatformColor = Int
 
 @ColorInt
-internal actual fun RGB.toPlatformColor() = Color.argb(alphaInt, redInt, greenInt, blueInt)
-internal actual fun PlatformColor.toRGB(): RGB =
-    RGB.from255(Color.red(this), Color.green(this), Color.blue(this), Color.alpha(this))
+internal actual fun RGB.toPlatformColor() = toColorInt()
+internal actual fun PlatformColor.toRGB() = RGB.fromColorInt(this)
