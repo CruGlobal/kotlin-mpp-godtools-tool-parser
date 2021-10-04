3a2329d1eea1a4d9a4a263f4c6fa3ffdd510f7a6
Daniel Frett <daniel.frett@ccci.org>
2021-10-04 10:48:53 -0400

    prepare for next development iteration

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 
 allprojects {
     group = "org.cru.godtools.kotlin"
-    version = "0.3.1"
+    version = "0.3.2-SNAPSHOT"
 
     repositories {
         maven("https://jitpack.io") {
