fb7f734407cb0176ab24203f9f45f12a630a56d5
Daniel Frett <daniel.frett@ccci.org>
2021-11-17 18:15:12 -0500

    enable build scans for JS Unit Tests job

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,3 +6,13 @@ include("module:state")
 include("module:expressions")
 
 include("test-fixtures")
+
+// automatically accept the scans.gradle.com TOS when running in GHA
+if (System.getenv("GITHUB_ACTIONS")?.toBoolean() == true) {
+    extensions.findByName("gradleEnterprise")?.withGroovyBuilder {
+        getProperty("buildScan").withGroovyBuilder {
+            setProperty("termsOfServiceUrl", "https://gradle.com/terms-of-service")
+            setProperty("termsOfServiceAgree", "yes")
+        }
+    }
+}
