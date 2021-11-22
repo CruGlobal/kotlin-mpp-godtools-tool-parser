8bbcb6959241af85579aaf31b73d9f89af68c9bc
Daniel Frett <daniel.frett@ccci.org>
2021-11-22 11:02:13 -0500

    add a parsing test of thefour in Korean

--- module/parser/src/commonTest/kotlin/org/cru/godtools/tool/service/ManifestParserTest.kt
+++ module/parser/src/commonTest/kotlin/org/cru/godtools/tool/service/ManifestParserTest.kt
@@ -5,6 +5,7 @@ import org.cru.godtools.tool.internal.RunOnAndroidWith
 import org.cru.godtools.tool.internal.TEST_XML_PULL_PARSER_FACTORY
 import org.cru.godtools.tool.internal.UsesResources
 import org.cru.godtools.tool.internal.runBlockingTest
+import org.cru.godtools.tool.xml.XmlPullParserFactory
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertIs
@@ -38,4 +39,26 @@ class ManifestParserTest : UsesResources("service") {
     fun testParseManifestInvalidPage() = runBlockingTest {
         assertIs<ParserResult.Error.Corrupted>(parser.parseManifest("manifest_invalid_page.xml"))
     }
+
+    // region Tool Parsing Tests
+    @Test
+    fun testParseManifestTheFourKo() = runBlockingTest {
+        assertIs<ParserResult.Data>(
+            parseTool(
+                "tools/thefour/ko/",
+                "c031c615dc52f6ebbb96076bef08351c5dab7edfd5f2a221905af707221cbdd0.xml"
+            )
+        )
+    }
+
+    private suspend fun parseTool(prefix: String, manifest: String) =
+        ManifestParser(PrefixXmlPullParserFactory(prefix, TEST_XML_PULL_PARSER_FACTORY)).parseManifest(manifest)
+
+    internal class PrefixXmlPullParserFactory(
+        private val prefix: String,
+        private val delegate: XmlPullParserFactory
+    ) : XmlPullParserFactory() {
+        override suspend fun getXmlParser(fileName: String) = delegate.getXmlParser("$prefix$fileName")
+    }
+    // endregion Tool Parsing Tests
 }
