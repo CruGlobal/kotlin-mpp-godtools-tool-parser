f0cd1d48dac3c370d6ef7d0fe18096a8b175e674
Daniel Frett <daniel.frett@ccci.org>
2021-12-02 11:57:06 -0500

    add support for parsing cards in a card collection page

--- module/parser/src/commonTest/kotlin/org/cru/godtools/tool/model/page/CardCollectionPageTest.kt
+++ module/parser/src/commonTest/kotlin/org/cru/godtools/tool/model/page/CardCollectionPageTest.kt
@@ -4,11 +4,17 @@ import org.cru.godtools.tool.internal.AndroidJUnit4
 import org.cru.godtools.tool.internal.RunOnAndroidWith
 import org.cru.godtools.tool.internal.UsesResources
 import org.cru.godtools.tool.internal.runBlockingTest
+import org.cru.godtools.tool.model.AnalyticsEvent.Trigger
 import org.cru.godtools.tool.model.Manifest
+import org.cru.godtools.tool.model.Paragraph
+import org.cru.godtools.tool.model.Spacer
+import org.cru.godtools.tool.model.TestColors
 import org.cru.godtools.tool.xml.XmlPullParserException
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertFailsWith
+import kotlin.test.assertIs
+import kotlin.test.assertTrue
 
 @RunOnAndroidWith(AndroidJUnit4::class)
 class CardCollectionPageTest : UsesResources("model/page") {
@@ -16,7 +22,28 @@ class CardCollectionPageTest : UsesResources("model/page") {
     @Test
     fun testParseCardCollectionPage() = runBlockingTest {
         with(CardCollectionPage(Manifest(), "file.ext", getTestXmlParser("page_cardcollection.xml"))) {
-            assertEquals(1, analyticsEvents.size)
+            val page = this
+            with(getAnalyticsEvents(Trigger.VISIBLE)) {
+                assertEquals(1, size)
+                assertEquals("page", this[0].action)
+            }
+            assertEquals(2, cards.size)
+            with(cards[0]) {
+                assertTrue(getAnalyticsEvents(Trigger.VISIBLE).isEmpty())
+                assertEquals(page.cardBackgroundColor, backgroundColor)
+                assertEquals(2, content.size)
+                assertIs<Spacer>(content[0])
+                assertIs<Paragraph>(content[1])
+            }
+            with(cards[1]) {
+                with(getAnalyticsEvents(Trigger.VISIBLE)) {
+                    assertEquals(1, size)
+                    assertEquals("card2", this[0].action)
+                }
+                assertTrue(getAnalyticsEvents(Trigger.HIDDEN).isEmpty())
+                assertEquals(TestColors.GREEN, backgroundColor)
+                assertTrue(content.isEmpty())
+            }
         }
     }
 
