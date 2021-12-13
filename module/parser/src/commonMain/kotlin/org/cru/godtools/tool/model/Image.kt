9edeb74b70b06c748708d112aa26774d34f730d7
Daniel Frett <daniel.frett@ccci.org>
2021-12-13 14:27:30 -0500

    support parsing width and gravity for images

--- module/parser/src/commonMain/kotlin/org/cru/godtools/tool/model/Image.kt
+++ module/parser/src/commonMain/kotlin/org/cru/godtools/tool/model/Image.kt
@@ -3,14 +3,22 @@ package org.cru.godtools.tool.model
 import org.cru.godtools.tool.internal.RestrictTo
 import org.cru.godtools.tool.internal.RestrictToScope
 import org.cru.godtools.tool.internal.VisibleForTesting
+import org.cru.godtools.tool.model.Dimension.Companion.toDimensionOrNull
+import org.cru.godtools.tool.model.Dimension.Pixels
+import org.cru.godtools.tool.model.Gravity.Companion.toGravityOrNull
 import org.cru.godtools.tool.xml.XmlPullParser
 import org.cru.godtools.tool.xml.skipTag
 
 private const val XML_RESOURCE = "resource"
+private const val XML_GRAVITY = "gravity"
+private const val XML_WIDTH = "width"
 
 class Image : Content, Clickable {
     internal companion object {
         internal const val XML_IMAGE = "image"
+
+        private val DEFAULT_GRAVITY = Gravity.Horizontal.CENTER
+        private val DEFAULT_WIDTH = Dimension.Percent(1f)
     }
 
     override val events: List<EventId>
@@ -19,6 +27,9 @@ class Image : Content, Clickable {
     @VisibleForTesting
     internal val resourceName: String?
     val resource get() = getResource(resourceName)
+
+    val gravity: Gravity.Horizontal
+    val width: Dimension
 
     internal constructor(parent: Base, parser: XmlPullParser) : super(parent, parser) {
         parser.require(XmlPullParser.START_TAG, XMLNS_CONTENT, XML_IMAGE)
@@ -29,6 +40,9 @@ class Image : Content, Clickable {
             this.url = url
         }
 
+        gravity = parser.getAttributeValue(XML_GRAVITY).toGravityOrNull()?.horizontal ?: DEFAULT_GRAVITY
+        width = parser.getAttributeValue(XML_WIDTH).toDimensionOrNull()?.takeIf { it is Pixels } ?: DEFAULT_WIDTH
+
         parser.skipTag()
     }
 
@@ -37,6 +51,8 @@ class Image : Content, Clickable {
     @RestrictTo(RestrictToScope.TESTS)
     constructor(parent: Base, resource: String? = null) : super(parent) {
         resourceName = resource
+        gravity = DEFAULT_GRAVITY
+        width = DEFAULT_WIDTH
         events = emptyList()
         url = null
     }
