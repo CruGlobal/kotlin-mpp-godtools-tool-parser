3d7be8a3f646c3f7c2951c1b5bb66410e2baf1b5
Daniel Frett <daniel.frett@ccci.org>
2022-01-14 16:46:14 -0500

    test parsing visibility attributes on Flow Items

--- module/parser/src/commonTest/kotlin/org/cru/godtools/tool/model/FlowTest.kt
+++ module/parser/src/commonTest/kotlin/org/cru/godtools/tool/model/FlowTest.kt
@@ -1,5 +1,6 @@
 package org.cru.godtools.tool.model
 
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
 import org.cru.godtools.tool.internal.AndroidJUnit4
 import org.cru.godtools.tool.internal.RunOnAndroidWith
@@ -7,19 +8,27 @@ import org.cru.godtools.tool.internal.UsesResources
 import org.cru.godtools.tool.model.Flow.Companion.DEFAULT_ITEM_WIDTH
 import org.cru.godtools.tool.model.Flow.Companion.DEFAULT_ROW_GRAVITY
 import org.cru.godtools.tool.model.tips.InlineTip
+import org.cru.godtools.tool.state.State
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFalse
 import kotlin.test.assertIs
+import kotlin.test.assertNull
 import kotlin.test.assertTrue
 
 @RunOnAndroidWith(AndroidJUnit4::class)
+@OptIn(ExperimentalCoroutinesApi::class)
 class FlowTest : UsesResources() {
+    private val state = State()
+
     @Test
     fun testParseFlowDefaults() = runTest {
         val flow = Flow(Manifest(), getTestXmlParser("flow_defaults.xml"))
         assertEquals(DEFAULT_ITEM_WIDTH, flow.itemWidth)
         assertEquals(DEFAULT_ROW_GRAVITY, flow.rowGravity)
         assertTrue(flow.items.isEmpty())
+        assertNull(flow.invisibleIf)
+        assertNull(flow.goneIf)
     }
 
     @Test
@@ -37,6 +46,21 @@ class FlowTest : UsesResources() {
         }
         assertIs<Paragraph>(flow.items[2].content.single())
         assertIs<InlineTip>(flow.items[3].content.single())
+    }
+
+    @Test
+    fun testParseFlowItemVisibility() = runTest {
+        val flow = Flow(Manifest(), getTestXmlParser("flow_visibility.xml"))
+        assertEquals(1, flow.items.size)
+        with(flow.items[0]) {
+            assertFalse(isInvisible(state))
+            state["invisible"] = "true"
+            assertTrue(isInvisible(state))
+
+            assertFalse(isGone(state))
+            state["hidden"] = "true"
+            assertTrue(isGone(state))
+        }
     }
 
     @Test
