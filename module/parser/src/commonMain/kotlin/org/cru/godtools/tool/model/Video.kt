90791a25d8bc18fe4a02234fd53d6329ffc67b9d
Daniel Frett <daniel.frett@ccci.org>
2022-01-31 14:08:11 -0500

    add support for parsing video aspect-ratio, width, and gravity

--- module/parser/src/commonMain/kotlin/org/cru/godtools/tool/model/Video.kt
+++ module/parser/src/commonMain/kotlin/org/cru/godtools/tool/model/Video.kt
@@ -1,15 +1,26 @@
 package org.cru.godtools.tool.model
 
+import org.cru.godtools.tool.model.AspectRatio.Companion.toAspectRatioOrNull
+import org.cru.godtools.tool.model.Dimension.Companion.toDimensionOrNull
+import org.cru.godtools.tool.model.Dimension.Pixels
+import org.cru.godtools.tool.model.Gravity.Companion.toGravityOrNull
 import org.cru.godtools.tool.xml.XmlPullParser
 import org.cru.godtools.tool.xml.skipTag
 
 private const val XML_VIDEO_ID = "video-id"
 private const val XML_PROVIDER = "provider"
 private const val XML_PROVIDER_YOUTUBE = "youtube"
+private const val XML_ASPECT_RATIO = "aspect-ratio"
+private const val XML_GRAVITY = "gravity"
+private const val XML_WIDTH = "width"
 
 class Video : Content {
     internal companion object {
         internal const val XML_VIDEO = "video"
+
+        internal val DEFAULT_ASPECT_RATIO = AspectRatio(16, 9)
+        internal val DEFAULT_GRAVITY = Gravity.Horizontal.CENTER
+        internal val DEFAULT_WIDTH = Dimension.Percent(1f)
     }
 
     enum class Provider {
@@ -28,13 +39,21 @@ class Video : Content {
     val provider: Provider
     val videoId: String?
 
+    val aspectRatio: AspectRatio
+    val gravity: Gravity.Horizontal
+    val width: Dimension
+
     override val isIgnored get() = super.isIgnored || provider == Provider.UNKNOWN || videoId == null
 
     internal constructor(parent: Base, parser: XmlPullParser) : super(parent, parser) {
         parser.require(XmlPullParser.START_TAG, XMLNS_CONTENT, XML_VIDEO)
 
-        provider = Provider.parseOrNull(parser.getAttributeValue(null, XML_PROVIDER)) ?: Provider.DEFAULT
-        videoId = parser.getAttributeValue(null, XML_VIDEO_ID)
+        provider = Provider.parseOrNull(parser.getAttributeValue(XML_PROVIDER)) ?: Provider.DEFAULT
+        videoId = parser.getAttributeValue(XML_VIDEO_ID)
+
+        aspectRatio = parser.getAttributeValue(XML_ASPECT_RATIO).toAspectRatioOrNull() ?: DEFAULT_ASPECT_RATIO
+        gravity = parser.getAttributeValue(XML_GRAVITY).toGravityOrNull()?.horizontal ?: DEFAULT_GRAVITY
+        width = parser.getAttributeValue(XML_WIDTH).toDimensionOrNull()?.takeIf { it is Pixels } ?: DEFAULT_WIDTH
 
         parser.skipTag()
     }
