ea0d398a779bf04c469b15f4e44ed36b4ce535fa
Daniel Frett <daniel.frett@ccci.org>
2022-03-01 16:52:38 -0500

    support specifying a textAlign override

--- module/parser/src/commonMain/kotlin/org/cru/godtools/tool/model/StylesOverride.kt
+++ module/parser/src/commonMain/kotlin/org/cru/godtools/tool/model/StylesOverride.kt
@@ -4,14 +4,17 @@ import org.cru.godtools.tool.model.Styles.Companion.DEFAULT_TEXT_SCALE
 
 private class StylesOverride(
     parent: Base,
+    private val _textAlign: (() -> Text.Align?)?,
     private val _textColor: (() -> PlatformColor?)?,
     private val _textScale: Double
 ) : BaseModel(parent), Styles {
+    override val textAlign get() = _textAlign?.invoke() ?: super.textAlign
     override val textColor get() = _textColor?.invoke() ?: super.textColor
     override val textScale get() = super.textScale * _textScale
 }
 
 internal fun Base.stylesOverride(
+    textAlign: (() -> Text.Align?)? = null,
     textColor: (() -> PlatformColor?)? = null,
     textScale: Double = DEFAULT_TEXT_SCALE
-): Base = StylesOverride(this, textColor, textScale)
+): Base = StylesOverride(this, textAlign, textColor, textScale)
