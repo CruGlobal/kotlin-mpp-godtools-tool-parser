476a9b25139f1c4322b348a2e6afc1a193065eb2
Daniel Frett <daniel.frett@ccci.org>
2022-04-22 14:39:56 -0400

    support a builder pattern for the ParserConfig on iOS

--- module/parser/src/commonMain/kotlin/org/cru/godtools/tool/ParserConfig.kt
+++ module/parser/src/commonMain/kotlin/org/cru/godtools/tool/ParserConfig.kt
@@ -7,9 +7,14 @@ const val FEATURE_CONTENT_CARD = "content_card"
 const val FEATURE_FLOW = "flow"
 const val FEATURE_MULTISELECT = "multiselect"
 
-class ParserConfig(
-    val supportedFeatures: Set<String> = emptySet(),
-    val supportedDeviceTypes: Set<DeviceType> = DEFAULT_SUPPORTED_DEVICE_TYPES
-)
+data class ParserConfig(
+    internal val supportedFeatures: Set<String> = emptySet(),
+    internal val supportedDeviceTypes: Set<DeviceType> = DEFAULT_SUPPORTED_DEVICE_TYPES
+) {
+    constructor() : this(supportedFeatures = emptySet())
+
+    fun withSupportedDeviceTypes(types: Set<DeviceType>) = copy(supportedDeviceTypes = types)
+    fun withSupportedFeatures(features: Set<String>) = copy(supportedFeatures = features)
+}
 
 internal expect val DEFAULT_SUPPORTED_DEVICE_TYPES: Set<DeviceType>
