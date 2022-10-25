4ced32f57c6e610e469e5a30454dcefb2b93eea3
Daniel Frett <daniel.frett@cru.org>
2022-10-25 13:51:32 -0400

    update the FlowWatcherTest to not require channels for testing watch()

--- module/parser/src/iosTest/kotlin/org/cru/godtools/tool/util/FlowWatcherTest.kt
+++ module/parser/src/iosTest/kotlin/org/cru/godtools/tool/util/FlowWatcherTest.kt
@@ -2,36 +2,32 @@ package org.cru.godtools.tool.util
 
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.consumeAsFlow
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.test.setMain
 import org.cru.godtools.tool.util.FlowWatcher.Companion.watch
 import kotlin.test.Test
 import kotlin.test.assertEquals
-import kotlin.test.assertTrue
 
 @OptIn(ExperimentalCoroutinesApi::class)
 class FlowWatcherTest {
     @Test
     fun testWatch() = runTest {
         Dispatchers.setMain(UnconfinedTestDispatcher())
-        val channel = Channel<Int>(1)
-        val output = Channel<Int>(1)
-        val watcher = channel.consumeAsFlow().watch {
-            output.trySend(it).getOrThrow()
+        var latestValue = 0
+        val source = MutableSharedFlow<Int>()
+        val watcher = source.watch {
+            latestValue = it
         }
 
-        channel.send(1)
-        assertEquals(1, output.receive(1000))
-        channel.send(2)
-        assertEquals(2, output.receive(1000))
+        source.emit(1)
+        assertEquals(1, latestValue)
+        source.emit(2)
+        assertEquals(2, latestValue)
 
         watcher.close()
-        channel.send(3)
-        delay(1000)
-        assertTrue(output.isEmpty)
+        source.emit(3)
+        assertEquals(2, latestValue)
     }
 }
