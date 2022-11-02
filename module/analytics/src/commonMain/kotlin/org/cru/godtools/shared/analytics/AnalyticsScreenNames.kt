e85156195938a4f5d89a0fff510cb03d6b7c26b1
Daniel Frett <daniel.frett@cru.org>
2022-11-02 16:51:34 -0400

    first pass at capturing analytics screen name constants

---
+++ module/analytics/src/commonMain/kotlin/org/cru/godtools/shared/analytics/AnalyticsScreenNames.kt
@@ -0,0 +1,29 @@
+package org.cru.godtools.shared.analytics
+
+object AnalyticsScreenNames {
+    // region Dashboard Screens
+    const val DASHBOARD_HOME = "Favorites"
+    const val DASHBOARD_ALL_TOOLS = "All Tools"
+    const val DASHBOARD_LESSONS = "Lessons"
+    // endregion Dashboard Screens
+
+    // region Account
+    const val ACCOUNT_GLOBAL_ACTIVITY = "Global Dashboard"
+    // endregion Account
+
+    // region Settings
+    const val SETTINGS_LANGUAGES = "Language Settings"
+    const val SETTINGS_LANGUAGE_SELECTION = "Select Language"
+    // endregion Settings
+
+    // region Miscellaneous Platform Menu Screens
+    const val PLATFORM_ABOUT = "About"
+    const val PLATFORM_HELP = "Help"
+    const val PLATFORM_CONTACT_US = "Contact Us"
+    const val PLATFORM_SHARE_GODTOOLS = "Share App"
+    const val PLATFORM_SHARE_STORY = "Share Story"
+    const val PLATFORM_TERMS_OF_USE = "Terms of Use"
+    const val PLATFORM_PRIVACY_POLICY = "Privacy Policy"
+    const val PLATFORM_COPYRIGHT = "Copyright Info"
+    // endregion Miscellaneous Platform Menu Screens
+}
