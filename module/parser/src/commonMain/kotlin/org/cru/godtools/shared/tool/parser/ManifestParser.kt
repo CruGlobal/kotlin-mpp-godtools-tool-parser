419a342c8b898f31b090dd7ee5ea377e4dbc9939
Daniel Frett <daniel.frett@cru.org>
2023-04-14 16:26:56 -0600

    switch to a configured parserDispatcher when parsing manifests

--- module/parser/src/commonMain/kotlin/org/cru/godtools/shared/tool/parser/ManifestParser.kt
+++ module/parser/src/commonMain/kotlin/org/cru/godtools/shared/tool/parser/ManifestParser.kt
@@ -3,6 +3,7 @@ package org.cru.godtools.shared.tool.parser
 import deezer.kustomexport.KustomExport
 import io.github.aakira.napier.Napier
 import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.withContext
 import org.cru.godtools.shared.tool.parser.internal.FileNotFoundException
 import org.cru.godtools.shared.tool.parser.model.Manifest
 import org.cru.godtools.shared.tool.parser.xml.XmlPullParserException
@@ -14,20 +15,26 @@ import kotlin.js.JsName
 @KustomExport
 open class ManifestParser(private val parserFactory: XmlPullParserFactory, val defaultConfig: ParserConfig) {
     suspend fun parseManifest(fileName: String) = parseManifest(fileName, defaultConfig)
-    suspend fun parseManifest(fileName: String, config: ParserConfig): ParserResult = try {
-        val manifest = Manifest.parse(fileName, config) {
-            parserFactory.getXmlParser(it)?.apply { nextTag() } ?: throw FileNotFoundException(fileName)
+    suspend fun parseManifest(
+        fileName: String,
+        config: ParserConfig,
+    ): ParserResult = withContext(config.parserDispatcher) {
+        try {
+            val manifest =
+                Manifest.parse(fileName, config) {
+                    parserFactory.getXmlParser(it)?.apply { nextTag() } ?: throw FileNotFoundException(fileName)
+                }
+            ParserResult.Data(manifest)
+        } catch (e: CancellationException) {
+            throw e
+        } catch (e: FileNotFoundException) {
+            ParserResult.Error.NotFound(e)
+        } catch (e: XmlPullParserException) {
+            ParserResult.Error.Corrupted(e)
+        } catch (e: Exception) {
+            Napier.e("Unexpected Parsing Exception", e, "ManifestParser")
+            ParserResult.Error(e)
         }
-        ParserResult.Data(manifest)
-    } catch (e: CancellationException) {
-        throw e
-    } catch (e: FileNotFoundException) {
-        ParserResult.Error.NotFound(e)
-    } catch (e: XmlPullParserException) {
-        ParserResult.Error.Corrupted(e)
-    } catch (e: Exception) {
-        Napier.e("Unexpected Parsing Exception", e, "ManifestParser")
-        ParserResult.Error(e)
     }
 }
 
