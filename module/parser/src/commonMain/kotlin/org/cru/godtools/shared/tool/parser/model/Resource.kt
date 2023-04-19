20533765f4a9a1c8e6f2ae925cea59ef8c7fe2f7
Daniel Frett <daniel.frett@cru.org>
2023-04-19 15:33:20 -0600

    export the Resource model to TypeScript

--- module/parser/src/commonMain/kotlin/org/cru/godtools/shared/tool/parser/model/Resource.kt
+++ module/parser/src/commonMain/kotlin/org/cru/godtools/shared/tool/parser/model/Resource.kt
@@ -4,10 +4,14 @@ import org.ccci.gto.support.androidx.annotation.RestrictTo
 import org.ccci.gto.support.androidx.annotation.RestrictToScope
 import org.cru.godtools.shared.tool.parser.xml.XmlPullParser
 import org.cru.godtools.shared.tool.parser.xml.skipTag
+import kotlin.js.ExperimentalJsExport
+import kotlin.js.JsExport
 
 private const val XML_FILENAME = "filename"
 private const val XML_SRC = "src"
 
+@JsExport
+@OptIn(ExperimentalJsExport::class)
 class Resource : BaseModel {
     internal companion object {
         internal const val XML_RESOURCE = "resource"
