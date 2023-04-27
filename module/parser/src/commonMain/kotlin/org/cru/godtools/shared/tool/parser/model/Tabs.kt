09497b2c60dfe1c00d827939e221c0cbf2bf4959
Daniel Frett <daniel.frett@cru.org>
2023-04-27 13:34:04 -0600

    export Tabs type to TypeScript

--- module/parser/src/commonMain/kotlin/org/cru/godtools/shared/tool/parser/model/Tabs.kt
+++ module/parser/src/commonMain/kotlin/org/cru/godtools/shared/tool/parser/model/Tabs.kt
@@ -7,15 +7,23 @@ import org.cru.godtools.shared.tool.parser.model.AnalyticsEvent.Companion.parseA
 import org.cru.godtools.shared.tool.parser.model.AnalyticsEvent.Trigger
 import org.cru.godtools.shared.tool.parser.xml.XmlPullParser
 import org.cru.godtools.shared.tool.parser.xml.parseChildren
+import kotlin.experimental.ExperimentalObjCRefinement
+import kotlin.js.ExperimentalJsExport
+import kotlin.js.JsExport
+import kotlin.js.JsName
+import kotlin.native.HiddenFromObjC
 
 private const val XML_TAB = "tab"
 private const val XML_LABEL = "label"
 
+@JsExport
+@OptIn(ExperimentalJsExport::class, ExperimentalObjCRefinement::class)
 class Tabs : Content {
     internal companion object {
         internal const val XML_TABS = "tabs"
     }
 
+    @JsName("_tabs")
     val tabs: List<Tab>
     override val tips get() = tabs.flatMap { it.contentTips }
 
@@ -37,6 +45,12 @@ class Tabs : Content {
     internal constructor(parent: Base = Manifest()) : super(parent) {
         tabs = emptyList()
     }
+
+    // region Kotlin/JS interop
+    @HiddenFromObjC
+    @JsName("tabs")
+    val jsTabs get() = tabs.toTypedArray()
+    // endregion Kotlin/JS interop
 
     class Tab : BaseModel, Parent, HasAnalyticsEvents {
         private val tabs: Tabs
