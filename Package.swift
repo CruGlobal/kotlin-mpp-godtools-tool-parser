e8c5cc9c2f26d286379f15cf91ac5ae596eaae7e
Levi Eggert <levi.eggert@cru.org>
2023-12-29 17:23:38 -0500

    Add Package.swift and .swiftpm

--- Package.swift
+++ Package.swift
@@ -1,20 +1,24 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "GodToolsShared",
-    platforms: [
-        .iOS(.v14)
-    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "GodToolsShared",
-            targets: ["GodToolsSharedBinaryPackage"]),
+            targets: ["GodToolsShared", "GodToolsSharedBinaryPackage"]),
     ],
     targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "GodToolsShared"),
+        .testTarget(
+            name: "GodToolsSharedTests",
+            dependencies: ["GodToolsShared"]),
         .binaryTarget(name: "GodToolsSharedBinaryPackage", path: "build/XCFrameworks/release/kotlin_mpp_godtools_tool_parser.xcframework")
     ]
 )
