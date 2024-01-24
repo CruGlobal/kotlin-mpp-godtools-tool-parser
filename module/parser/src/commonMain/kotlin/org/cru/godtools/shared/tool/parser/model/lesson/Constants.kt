f35f91ec032669bf2ba4930b7b797f8bc142af7b
Daniel Frett <daniel.frett@cru.org>
2024-01-23 17:09:19 -0700

    remove deprecated SharedImmutable annotation
    this is no longer needed with the new memory model

--- module/parser/src/commonMain/kotlin/org/cru/godtools/shared/tool/parser/model/lesson/Constants.kt
+++ module/parser/src/commonMain/kotlin/org/cru/godtools/shared/tool/parser/model/lesson/Constants.kt
@@ -1,11 +1,9 @@
 package org.cru.godtools.shared.tool.parser.model.lesson
 
-import kotlin.native.concurrent.SharedImmutable
 import org.cru.godtools.shared.tool.parser.internal.AndroidColorInt
 import org.cru.godtools.shared.tool.parser.model.color
 
 internal const val XMLNS_LESSON = "https://mobile-content-api.cru.org/xmlns/lesson"
 
 @AndroidColorInt
-@SharedImmutable
 internal val DEFAULT_LESSON_NAV_BAR_COLOR = color(0, 0, 0, 0.0)
--- module/parser/src/commonMain/kotlin/org/cru/godtools/shared/tool/parser/model/page/Constants.kt
+++ module/parser/src/commonMain/kotlin/org/cru/godtools/shared/tool/parser/model/page/Constants.kt
@@ -1,6 +1,5 @@
 package org.cru.godtools.shared.tool.parser.model.page
 
-import kotlin.native.concurrent.SharedImmutable
 import org.cru.godtools.shared.tool.parser.internal.AndroidColorInt
 import org.cru.godtools.shared.tool.parser.model.color
 
@@ -10,5 +9,4 @@ internal const val XMLNS_XSI = "http://www.w3.org/2001/XMLSchema-instance"
 internal const val XML_CONTROL_COLOR = "control-color"
 
 @AndroidColorInt
-@SharedImmutable
 internal val DEFAULT_CONTROL_COLOR = color(225, 225, 225, 1.0)
