3623897acc9809f41e4834f62feb4ee2258fce38
Daniel Frett <daniel.frett@cru.org>
2024-10-16 11:11:56 -0600

    make Base.parent visible for use in the godtools-shared library

--- module/parser/src/commonMain/kotlin/org/cru/godtools/shared/tool/parser/model/Base.kt
+++ module/parser/src/commonMain/kotlin/org/cru/godtools/shared/tool/parser/model/Base.kt
@@ -1,6 +1,18 @@
 package org.cru.godtools.shared.tool.parser.model
 
+import kotlin.experimental.ExperimentalObjCRefinement
+import kotlin.js.ExperimentalJsExport
+import kotlin.js.JsExport
+import kotlin.native.HiddenFromObjC
+import org.ccci.gto.support.androidx.annotation.RestrictTo
+import org.ccci.gto.support.androidx.annotation.RestrictToScope
+
+@OptIn(ExperimentalJsExport::class, ExperimentalObjCRefinement::class)
 interface Base {
+    @HiddenFromObjC
+    @JsExport.Ignore
+    @get:RestrictTo(RestrictToScope.LIBRARY)
+    val parent: Base? get() = null
     val manifest: Manifest
     val stylesParent: Styles?
 }
