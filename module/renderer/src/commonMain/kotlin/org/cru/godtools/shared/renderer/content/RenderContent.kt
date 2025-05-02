49e96a5105e18f6ec175be76783858a02e802d92
Daniel Frett <daniel.frett@cru.org>
2025-05-02 13:17:40 -0600

    create a renderer module that will use Compose to render a content stack

---
+++ module/renderer/src/commonMain/kotlin/org/cru/godtools/shared/renderer/content/RenderContent.kt
@@ -0,0 +1,24 @@
+package org.cru.godtools.shared.renderer.content
+
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import org.cru.godtools.shared.tool.parser.model.Content
+import org.cru.godtools.shared.tool.parser.model.Text
+
+@Composable
+internal fun ColumnScope.RenderContent(content: List<Content>) {
+    content.forEach { RenderContent(it) }
+}
+
+@Composable
+internal fun ColumnScope.RenderContent(content: Content) {
+    when (content) {
+        is Text -> RenderText(content)
+        else -> Text(
+            "Unsupported Content Element: ${content::class.simpleName}",
+            color = Color.Red,
+        )
+    }
+}
