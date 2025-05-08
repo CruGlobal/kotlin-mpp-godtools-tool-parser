75358079830c5214d8edc02ed28a65ba9117450c
Daniel Frett <daniel.frett@cru.org>
2025-05-08 17:20:58 -0600

    convert navbar colors to colormath Color types

--- module/parser/src/commonMain/kotlin/org/cru/godtools/shared/tool/parser/model/lesson/Constants.kt
+++ module/parser/src/commonMain/kotlin/org/cru/godtools/shared/tool/parser/model/lesson/Constants.kt
@@ -2,9 +2,8 @@ package org.cru.godtools.shared.tool.parser.model.lesson
 
 import org.cru.godtools.shared.tool.parser.internal.AndroidColorInt
 import org.cru.godtools.shared.tool.parser.internal.color
-import org.cru.godtools.shared.tool.parser.model.toPlatformColor
 
 internal const val XMLNS_LESSON = "https://mobile-content-api.cru.org/xmlns/lesson"
 
 @AndroidColorInt
-internal val DEFAULT_LESSON_NAV_BAR_COLOR = color(0, 0, 0, 0.0).toPlatformColor()
+internal val DEFAULT_LESSON_NAV_BAR_COLOR = color(0, 0, 0, 0.0)
