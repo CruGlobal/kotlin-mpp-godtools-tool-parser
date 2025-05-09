e992ffc75b18b8b4fedfa20e8d7c5869125a9602
Daniel Frett <daniel.frett@cru.org>
2025-05-09 11:04:51 -0600

    update CallToAction.controlColor to be a colormath Color

--- module/parser/src/commonMain/kotlin/org/cru/godtools/shared/tool/parser/model/tract/CallToAction.kt
+++ module/parser/src/commonMain/kotlin/org/cru/godtools/shared/tool/parser/model/tract/CallToAction.kt
@@ -2,19 +2,18 @@ package org.cru.godtools.shared.tool.parser.model.tract
 
 import androidx.annotation.RestrictTo
 import androidx.annotation.VisibleForTesting
+import com.github.ajalt.colormath.Color
 import kotlin.js.ExperimentalJsExport
 import kotlin.js.JsExport
 import kotlin.js.JsName
 import org.cru.godtools.shared.tool.parser.internal.AndroidColorInt
 import org.cru.godtools.shared.tool.parser.internal.toColorOrNull
 import org.cru.godtools.shared.tool.parser.model.BaseModel
-import org.cru.godtools.shared.tool.parser.model.PlatformColor
 import org.cru.godtools.shared.tool.parser.model.Text
 import org.cru.godtools.shared.tool.parser.model.parseTextChild
 import org.cru.godtools.shared.tool.parser.model.primaryColor
 import org.cru.godtools.shared.tool.parser.model.stylesParent
 import org.cru.godtools.shared.tool.parser.model.tips.XMLNS_TRAINING
-import org.cru.godtools.shared.tool.parser.model.toPlatformColor
 import org.cru.godtools.shared.tool.parser.xml.XmlPullParser
 
 private const val XML_CONTROL_COLOR = "control-color"
@@ -31,11 +30,9 @@ class CallToAction : BaseModel {
 
     val label: Text?
 
-    @AndroidColorInt
     @Suppress("ktlint:standard:property-naming") // https://github.com/pinterest/ktlint/issues/2448
-    private val _controlColor: PlatformColor?
-    @get:AndroidColorInt
-    internal val controlColor get() = _controlColor ?: stylesParent.primaryColor.toPlatformColor()
+    private val _controlColor: Color?
+    internal val controlColor get() = _controlColor ?: stylesParent.primaryColor
 
     @VisibleForTesting
     internal val tipId: String?
@@ -52,7 +49,7 @@ class CallToAction : BaseModel {
         parser.require(XmlPullParser.START_TAG, XMLNS_TRACT, XML_CALL_TO_ACTION)
 
         this.page = page
-        _controlColor = parser.getAttributeValue(XML_CONTROL_COLOR)?.toColorOrNull()?.toPlatformColor()
+        _controlColor = parser.getAttributeValue(XML_CONTROL_COLOR)?.toColorOrNull()
         tipId = parser.getAttributeValue(XMLNS_TRAINING, XML_TIP)
 
         label = parser.parseTextChild(this, XMLNS_TRACT, XML_CALL_TO_ACTION)
@@ -63,7 +60,7 @@ class CallToAction : BaseModel {
     constructor(
         page: TractPage = TractPage(),
         label: ((CallToAction) -> Text)? = null,
-        @AndroidColorInt controlColor: PlatformColor? = null,
+        controlColor: Color? = null,
         tip: String? = null
     ) : super(page) {
         this.page = page
@@ -74,4 +71,4 @@ class CallToAction : BaseModel {
 }
 
 @get:AndroidColorInt
-val CallToAction?.controlColor get() = this?.controlColor ?: stylesParent.primaryColor.toPlatformColor()
+val CallToAction?.controlColor get() = this?.controlColor ?: stylesParent.primaryColor
