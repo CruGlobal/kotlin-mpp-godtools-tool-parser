6deb29fef70f32a793c42a85634acf89736b95d4
Daniel Frett <daniel.frett@cru.org>
2025-05-16 16:58:40 -0600

    add RenderButtonPaparazziTest

--- module/renderer/src/commonMain/kotlin/org/cru/godtools/shared/renderer/content/RenderButton.kt
+++ module/renderer/src/commonMain/kotlin/org/cru/godtools/shared/renderer/content/RenderButton.kt
@@ -1,8 +1,11 @@
 package org.cru.godtools.shared.renderer.content
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.dp
+import com.github.ajalt.colormath.extensions.android.composecolor.toComposeColor
 import org.cru.godtools.shared.tool.parser.model.Button
 
 @Composable
@@ -10,15 +13,19 @@ internal fun RenderButton(button: Button) {
     Button(
         onClick = { },
         colors = when (button.style) {
-            Button.Style.OUTLINED -> ButtonDefaults.outlinedButtonColors()
-            Button.Style.CONTAINED, Button.Style.UNKNOWN -> ButtonDefaults.buttonColors()
+            Button.Style.OUTLINED -> ButtonDefaults.outlinedButtonColors(
+                containerColor = button.backgroundColor.toComposeColor()
+            )
+            Button.Style.CONTAINED, Button.Style.UNKNOWN -> ButtonDefaults.buttonColors(
+                containerColor = button.buttonColor.toComposeColor()
+            )
         },
         elevation = when (button.style) {
             Button.Style.OUTLINED -> null
             Button.Style.CONTAINED, Button.Style.UNKNOWN -> ButtonDefaults.buttonElevation()
         },
         border = when (button.style) {
-            Button.Style.OUTLINED -> ButtonDefaults.outlinedButtonBorder()
+            Button.Style.OUTLINED -> BorderStroke(1.dp, button.buttonColor.toComposeColor())
             Button.Style.CONTAINED, Button.Style.UNKNOWN -> null
         }
     ) {
