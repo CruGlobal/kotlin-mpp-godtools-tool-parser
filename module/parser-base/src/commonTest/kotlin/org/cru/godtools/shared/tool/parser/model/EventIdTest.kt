426922aa441a2bf25f92cdcd6a88f9e20d7e1241
Daniel Frett <daniel.frett@cru.org>
2025-05-19 15:43:50 -0600

    add a few more EventId tests to hopefully improve coverage

--- module/parser-base/src/commonTest/kotlin/org/cru/godtools/shared/tool/parser/model/EventIdTest.kt
+++ module/parser-base/src/commonTest/kotlin/org/cru/godtools/shared/tool/parser/model/EventIdTest.kt
@@ -2,6 +2,7 @@ package org.cru.godtools.shared.tool.parser.model
 
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFalse
 import kotlin.test.assertNotEquals
 import kotlin.test.assertTrue
 import org.cru.godtools.shared.tool.state.State
@@ -14,6 +15,7 @@ class EventIdTest {
     @Test
     fun verifyHashCode() {
         assertEquals(EventId.FOLLOWUP.hashCode(), ID1.hashCode())
+        assertEquals(EventId(name = "ABC"), EventId(name = "abc"))
     }
 
     @Test
@@ -21,6 +23,13 @@ class EventIdTest {
         assertEquals(EventId.FOLLOWUP, ID1)
         assertNotEquals(EventId.FOLLOWUP, EventId("followup", "invalid"))
         assertNotEquals(EventId.FOLLOWUP, EventId(null, "send"))
+        assertFalse(EventId.FOLLOWUP == Any())
+    }
+
+    @Test
+    fun verifyToString() {
+        assertEquals("followup:send", EventId.FOLLOWUP.toString())
+        assertEquals("abc", EventId(name = "abc").toString())
     }
 
     // region resolve(State)
