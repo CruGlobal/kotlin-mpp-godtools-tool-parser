a6321bc7ae235cbff43c98cd8c828519074e082b
Daniel Frett <daniel.frett@cru.org>
2025-05-23 11:05:38 -0600

    define a PARAGRAPH_VERTICAL_PADDING constant

--- module/renderer/src/commonMain/kotlin/org/cru/godtools/shared/renderer/content/RenderParagraph.kt
+++ module/renderer/src/commonMain/kotlin/org/cru/godtools/shared/renderer/content/RenderParagraph.kt
@@ -8,9 +8,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import org.cru.godtools.shared.tool.parser.model.Paragraph
 
+private val PARAGRAPH_VERTICAL_PADDING = 10.dp
+
 @Composable
 internal fun ColumnScope.RenderParagraph(paragraph: Paragraph) {
-    Spacer(Modifier.height(10.dp))
+    Spacer(Modifier.height(PARAGRAPH_VERTICAL_PADDING))
     RenderContent(paragraph.content)
-    Spacer(Modifier.height(10.dp))
+    Spacer(Modifier.height(PARAGRAPH_VERTICAL_PADDING))
 }
