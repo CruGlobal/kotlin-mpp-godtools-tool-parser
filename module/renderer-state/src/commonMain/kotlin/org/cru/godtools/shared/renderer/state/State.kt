2a7c717513bc3ef10700bb2195cfd97880155fbf
Daniel Frett <daniel.frett@cru.org>
2025-05-23 16:54:04 -0600

    add a State.contentEvents shared flow for emitting triggered content events

--- module/renderer-state/src/commonMain/kotlin/org/cru/godtools/shared/renderer/state/State.kt
+++ module/renderer-state/src/commonMain/kotlin/org/cru/godtools/shared/renderer/state/State.kt
@@ -1,19 +1,24 @@
 package org.cru.godtools.shared.renderer.state
 
 import androidx.annotation.RestrictTo
+import kotlin.coroutines.EmptyCoroutineContext
 import kotlin.experimental.ExperimentalObjCRefinement
 import kotlin.js.ExperimentalJsExport
 import kotlin.js.JsExport
 import kotlin.js.JsName
 import kotlin.native.HiddenFromObjC
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.conflate
 import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onSubscription
+import kotlinx.coroutines.launch
 import org.cru.godtools.shared.renderer.state.internal.Parcelable
 import org.cru.godtools.shared.renderer.state.internal.Parcelize
+import org.cru.godtools.shared.tool.parser.model.EventId
 
 @JsExport
 @Parcelize
@@ -24,6 +29,14 @@ class State internal constructor(
 ) : Parcelable {
     @JsName("createState")
     constructor() : this(vars = mutableMapOf())
+
+    private var coroutineScope = CoroutineScope(EmptyCoroutineContext)
+    @HiddenFromObjC
+    @JsExport.Ignore
+    @RestrictTo(RestrictTo.Scope.TESTS)
+    fun setTestCoroutineScope(scope: CoroutineScope) {
+        coroutineScope = scope
+    }
 
     // region Analytics Events Tracking
     @HiddenFromObjC
@@ -76,4 +89,18 @@ class State internal constructor(
         if (values.contains(value)) setVar(key, values.filterNot { it == value })
     }
     // endregion State vars
+
+    // region Content Events
+    private val _contentEvents = MutableSharedFlow<EventId>()
+    val contentEvents = _contentEvents.asSharedFlow()
+
+    internal fun resolveContentEvent(eventId: EventId) = when (eventId.namespace) {
+        EventId.NAMESPACE_STATE -> getVar(eventId.name).map { EventId(name = it) }
+        else -> listOf(eventId)
+    }
+
+    fun triggerContentEvents(events: List<EventId>) {
+        coroutineScope.launch { events.flatMap { resolveContentEvent(it) }.forEach { _contentEvents.emit(it) } }
+    }
+    // endregion Content Events
 }
