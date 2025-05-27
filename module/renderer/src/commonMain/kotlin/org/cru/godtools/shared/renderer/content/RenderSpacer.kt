b925e11b2863b5dab693c2d23c52bfab8a2ce081
Levi Eggert <levi.eggert@cru.org>
2025-05-27 12:31:19 -0400

    Change RenderSpacer to use ColumnScope

--- module/renderer/src/commonMain/kotlin/org/cru/godtools/shared/renderer/content/RenderSpacer.kt
+++ module/renderer/src/commonMain/kotlin/org/cru/godtools/shared/renderer/content/RenderSpacer.kt
@@ -1,6 +1,6 @@
 package org.cru.godtools.shared.renderer.content
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.layout.height
@@ -9,10 +9,10 @@ import androidx.compose.ui.Modifier
 import org.cru.godtools.shared.tool.parser.model.Spacer
 
 @Composable
-internal fun RenderSpacer(spacer: Spacer, modifier: Modifier = Modifier) {
+internal fun ColumnScope.RenderSpacer(spacer: Spacer, modifier: Modifier = Modifier) {
 
     when (spacer.mode) {
         Spacer.Mode.FIXED -> Spacer(Modifier.height(spacer.height.dp))
-        Spacer.Mode.AUTO -> Column { Spacer(modifier = modifier.weight(1.0f)) }
+        Spacer.Mode.AUTO -> Spacer(modifier = modifier.weight(1.0f))
     }
 }
