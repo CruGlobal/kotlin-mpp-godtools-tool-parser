18e8298eeeac3906dbfd4ff1edddf33b893c3622
Levi Eggert <levi.eggert@cru.org>
2025-05-27 08:33:56 -0400

    Add spacer to render content

--- module/renderer/src/commonMain/kotlin/org/cru/godtools/shared/renderer/content/RenderContent.kt
+++ module/renderer/src/commonMain/kotlin/org/cru/godtools/shared/renderer/content/RenderContent.kt
@@ -6,6 +6,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import org.cru.godtools.shared.tool.parser.model.Content
 import org.cru.godtools.shared.tool.parser.model.Paragraph
+import org.cru.godtools.shared.tool.parser.model.Spacer
 import org.cru.godtools.shared.tool.parser.model.Text
 
 @Composable
@@ -18,6 +19,7 @@ internal fun ColumnScope.RenderContent(content: Content) {
     when (content) {
         is Paragraph -> RenderParagraph(content)
         is Text -> RenderText(content)
+        is Spacer -> RenderSpacer(content)
         else -> Text(
             "Unsupported Content Element: ${content::class.simpleName}",
             color = Color.Red,
