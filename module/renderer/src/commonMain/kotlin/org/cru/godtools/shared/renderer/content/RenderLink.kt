8cf9e158116ce37e2b9377e36e5f35768b0ca534
Levi Eggert <levi.eggert@cru.org>
2025-05-28 17:48:50 -0400

    Add modifier

--- module/renderer/src/commonMain/kotlin/org/cru/godtools/shared/renderer/content/RenderLink.kt
+++ module/renderer/src/commonMain/kotlin/org/cru/godtools/shared/renderer/content/RenderLink.kt
@@ -1,10 +1,20 @@
 package org.cru.godtools.shared.renderer.content
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import org.cru.godtools.shared.tool.parser.model.Link
 
 @Composable
-internal fun ColumnScope.RenderLink(link: Link) {
-    RenderText(link.text)
+fun ColumnScope.RenderLink(link: Link, modifier: Modifier = Modifier) {
+    RenderTextNode(
+        link.text,
+        modifier = modifier
+            .padding(horizontal = Horizontal_Padding)
+            .fillMaxWidth()
+            .clickable { /* TODO */ }
+    )
 }
