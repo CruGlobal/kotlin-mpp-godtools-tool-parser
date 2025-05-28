7994f784387aab4a50cb23ec06d344e56e84db18
Levi Eggert <levi.eggert@cru.org>
2025-05-28 08:37:56 -0400

    Reorder imports Remove Modifier argument

--- module/renderer/src/commonMain/kotlin/org/cru/godtools/shared/renderer/content/RenderSpacer.kt
+++ module/renderer/src/commonMain/kotlin/org/cru/godtools/shared/renderer/content/RenderSpacer.kt
@@ -1,18 +1,17 @@
 package org.cru.godtools.shared.renderer.content
 
 import androidx.compose.foundation.layout.ColumnScope
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import org.cru.godtools.shared.tool.parser.model.Spacer
 
 @Composable
-internal fun ColumnScope.RenderSpacer(spacer: Spacer, modifier: Modifier = Modifier) {
-
+internal fun ColumnScope.RenderSpacer(spacer: Spacer) {
     when (spacer.mode) {
         Spacer.Mode.FIXED -> Spacer(Modifier.height(spacer.height.dp))
-        Spacer.Mode.AUTO -> Spacer(modifier = modifier.weight(1.0f))
+        Spacer.Mode.AUTO -> Spacer(Modifier.weight(1.0f))
     }
 }
