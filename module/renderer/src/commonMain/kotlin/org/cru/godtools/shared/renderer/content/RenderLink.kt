1e2a03d57fad04988c591f39f48316b379bdc6f2
Levi Eggert <levi.eggert@cru.org>
2025-05-28 17:22:11 -0400

    Remove RenderLinkNode

--- module/renderer/src/commonMain/kotlin/org/cru/godtools/shared/renderer/content/RenderLink.kt
+++ module/renderer/src/commonMain/kotlin/org/cru/godtools/shared/renderer/content/RenderLink.kt
@@ -1,21 +1,10 @@
 package org.cru.godtools.shared.renderer.content
 
 import androidx.compose.foundation.layout.ColumnScope
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import org.cru.godtools.shared.tool.parser.model.Link
 
 @Composable
-internal fun ColumnScope.RenderLink(link: Link) = RenderLinkNode(
-    link,
-    Modifier
-        .padding(horizontal = Horizontal_Padding)
-        .fillMaxWidth(),
-)
-
-@Composable
-internal fun RenderLinkNode(link: Link, modifier: Modifier = Modifier) {
-    RenderTextNode(link.text, modifier)
+internal fun ColumnScope.RenderLink(link: Link) {
+    RenderText(link.text)
 }
