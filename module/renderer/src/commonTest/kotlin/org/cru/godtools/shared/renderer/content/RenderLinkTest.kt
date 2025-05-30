4e1873ab6d5c77ac5a80084b04a307b9e5ec4dc1
Levi Eggert <levi.eggert@cru.org>
2025-05-30 16:26:37 -0400

    Add RenderLinkTest

---
+++ module/renderer/src/commonTest/kotlin/org/cru/godtools/shared/renderer/content/RenderLinkTest.kt
@@ -0,0 +1,21 @@
+package org.cru.godtools.shared.renderer.content
+
+import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.SemanticsNodeInteractionsProvider
+import androidx.compose.ui.test.onNodeWithText
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.ccci.gto.support.androidx.test.junit.runners.AndroidJUnit4
+import org.ccci.gto.support.androidx.test.junit.runners.RunOnAndroidWith
+import org.cru.godtools.shared.tool.parser.model.Link
+import org.cru.godtools.shared.tool.parser.model.Text
+
+@RunOnAndroidWith(AndroidJUnit4::class)
+@OptIn(ExperimentalCoroutinesApi::class, ExperimentalTestApi::class)
+class RenderLinkTest : BaseRenderContentTest() {
+    override val testModel = Link(
+        text = { Text(text = "Test") },
+        events = clickableEvents,
+        url = clickableUrl
+    )
+    override fun SemanticsNodeInteractionsProvider.onModelNode() = onNodeWithText("Test")
+}
