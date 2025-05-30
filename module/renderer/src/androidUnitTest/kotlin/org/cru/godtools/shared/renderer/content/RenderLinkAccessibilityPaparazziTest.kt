6fb851c179bec88ef47b6f31f57ad0ccba6e3f59
Levi Eggert <levi.eggert@cru.org>
2025-05-30 17:15:36 -0400

    Add accessibility screenshot test

--- module/renderer/src/androidUnitTest/kotlin/org/cru/godtools/shared/renderer/content/RenderLinkAccessibilityPaparazziTest.kt
+++ module/renderer/src/androidUnitTest/kotlin/org/cru/godtools/shared/renderer/content/RenderLinkAccessibilityPaparazziTest.kt
@@ -1,7 +1,19 @@
 package org.cru.godtools.shared.renderer.content
 
+import kotlin.test.Test
 import org.cru.godtools.shared.renderer.BasePaparazziTest
+import org.cru.godtools.shared.tool.parser.model.Link
+import org.cru.godtools.shared.tool.parser.model.Text
 
 class RenderLinkAccessibilityPaparazziTest : BasePaparazziTest(accessibilityMode = AccessibilityMode.ACCESSIBILITY) {
-
+    @Test
+    fun `RenderLink() - TextAlign`() = contentSnapshot {
+        RenderContentStack(
+            listOf(
+                Link(
+                    text = { Text(it, "Align Start", textAlign = Text.Align.START) }
+                )
+            )
+        )
+    }
 }
