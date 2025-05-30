102e5c3105d2c24fac3c2e7c56b82aadc0672293
Levi Eggert <levi.eggert@cru.org>
2025-05-30 16:43:48 -0400

    Add file RenderLinkAccessibilityPaparazziTest

---
+++ module/renderer/src/androidUnitTest/kotlin/org/cru/godtools/shared/renderer/content/RenderLinkAccessibilityPaparazziTest.kt
@@ -0,0 +1,7 @@
+package org.cru.godtools.shared.renderer.content
+
+import org.cru.godtools.shared.renderer.BasePaparazziTest
+
+class RenderLinkAccessibilityPaparazziTest : BasePaparazziTest(accessibilityMode = AccessibilityMode.ACCESSIBILITY) {
+
+}
