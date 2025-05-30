033e9a7aa2d20fe0834fa5ab5ad232690193ce23
Levi Eggert <levi.eggert@cru.org>
2025-05-30 08:54:37 -0400

    Use minimum interactive component size instead of heightIn min Align link based on model text alignment

--- module/renderer/src/commonMain/kotlin/org/cru/godtools/shared/renderer/content/RenderLink.kt
+++ module/renderer/src/commonMain/kotlin/org/cru/godtools/shared/renderer/content/RenderLink.kt
@@ -4,18 +4,16 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.minimumInteractiveComponentSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+import org.cru.godtools.shared.renderer.content.extensions.alignment
 import org.cru.godtools.shared.renderer.content.extensions.handleClickable
+import org.cru.godtools.shared.renderer.content.extensions.textAlign
 import org.cru.godtools.shared.renderer.state.State
 import org.cru.godtools.shared.tool.parser.model.Link
-
-private val Link_Minimum_Height = 48.dp
 
 @Composable
 fun ColumnScope.RenderLink(link: Link, state: State, modifier: Modifier = Modifier) {
@@ -23,17 +21,15 @@ fun ColumnScope.RenderLink(link: Link, state: State, modifier: Modifier = Modifi
 
     Box(
         modifier = Modifier
-            .clickable { link.handleClickable(state, scope) }
             .fillMaxWidth()
-            .heightIn(min = Link_Minimum_Height),
-        contentAlignment = Alignment.Center
+            .minimumInteractiveComponentSize()
     ) {
-
         RenderTextNode(
             link.text,
             modifier = modifier
                 .padding(horizontal = Horizontal_Padding)
-                .fillMaxWidth()
+                .align(link.text.textAlign.textAlign.alignment)
+                .clickable { link.handleClickable(state, scope) }
         )
     }
 }
