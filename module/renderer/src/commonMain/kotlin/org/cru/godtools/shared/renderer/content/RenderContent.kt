3cc9f1d4859615f9161a678262f64b9385848ac8
Levi Eggert <levi.eggert@cru.org>
2025-06-10 08:34:17 -0400

    Skip rendering if visibility is equal to gone Create modifier for content

--- module/renderer/src/commonMain/kotlin/org/cru/godtools/shared/renderer/content/RenderContent.kt
+++ module/renderer/src/commonMain/kotlin/org/cru/godtools/shared/renderer/content/RenderContent.kt
@@ -3,7 +3,9 @@ package org.cru.godtools.shared.renderer.content
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import org.cru.godtools.shared.renderer.content.extensions.contentModel
 import org.cru.godtools.shared.renderer.state.State
 import org.cru.godtools.shared.tool.parser.model.Button
 import org.cru.godtools.shared.tool.parser.model.Content
@@ -11,14 +13,32 @@ import org.cru.godtools.shared.tool.parser.model.Link
 import org.cru.godtools.shared.tool.parser.model.Paragraph
 import org.cru.godtools.shared.tool.parser.model.Spacer
 import org.cru.godtools.shared.tool.parser.model.Text
+import org.cru.godtools.shared.tool.parser.model.Visibility
+import org.cru.godtools.shared.tool.parser.model.VisibilityEnum
 
 @Composable
 internal fun ColumnScope.RenderContent(content: List<Content>, state: State) {
-    content.forEach { RenderContent(it, state) }
+    content.forEach {
+
+        val visibility: VisibilityEnum = (content as? Visibility)?.let { it
+            it.getVisibility(state = state)
+        } ?: run {
+            VisibilityEnum.VISIBLE
+        }
+
+        val isRenderable: Boolean = visibility != VisibilityEnum.GONE
+
+        if (isRenderable) {
+            RenderContent(it, state, visibility)
+        }
+    }
 }
 
 @Composable
-internal fun ColumnScope.RenderContent(content: Content, state: State) {
+internal fun ColumnScope.RenderContent(content: Content, state: State, visibility: VisibilityEnum) {
+
+    val modifier = Modifier.contentModel(model = content, visibility = visibility)
+
     when (content) {
         is Button -> RenderButton(content, state)
         is Paragraph -> RenderParagraph(content, state)
