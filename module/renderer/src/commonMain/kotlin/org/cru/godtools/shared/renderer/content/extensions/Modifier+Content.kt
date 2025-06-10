d489b77f13713762ada029a33173d99352fd35ad
Levi Eggert <levi.eggert@cru.org>
2025-06-10 13:59:26 -0400

    Remove composed

--- module/renderer/src/commonMain/kotlin/org/cru/godtools/shared/renderer/content/extensions/Modifier+Content.kt
+++ module/renderer/src/commonMain/kotlin/org/cru/godtools/shared/renderer/content/extensions/Modifier+Content.kt
@@ -1,22 +1,15 @@
 package org.cru.godtools.shared.renderer.content.extensions
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.composed
 import org.cru.godtools.shared.renderer.state.State
 import org.cru.godtools.shared.tool.parser.model.Content
 import org.cru.godtools.shared.tool.parser.model.Visibility
-import org.cru.godtools.shared.tool.parser.model.VisibilityEnum
 
-fun Modifier.content(content: Content, state: State) =
-    composed(
-        factory = {
-            val visibility: VisibilityEnum = (content as? Visibility)?.let { it
-                it.getVisibility(state = state)
-            } ?: run {
-                VisibilityEnum.VISIBLE
-            }
+@Composable
+fun Modifier.content(content: Content, state: State) {
 
-            Modifier
-                .visibility(visibility)
-        }
-    )
+    Modifier
+        .invisibleIf(invisible = (content as? Visibility)?.isInvisibleFlow(state = state)?.collectAsState(false)?.value ?: false)
+}
--- module/renderer/src/commonMain/kotlin/org/cru/godtools/shared/renderer/content/extensions/Modifier+Visibility.kt
+++ module/renderer/src/commonMain/kotlin/org/cru/godtools/shared/renderer/content/extensions/Modifier+Visibility.kt
@@ -1,17 +1,9 @@
 package org.cru.godtools.shared.renderer.content.extensions
 
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.composed
-import androidx.compose.ui.draw.alpha
-import org.cru.godtools.shared.tool.parser.model.VisibilityEnum
+import androidx.compose.ui.draw.drawWithContent
 
-fun Modifier.visibility(visibility: VisibilityEnum) =
-    composed(
-        factory = {
-            when (visibility) {
-                VisibilityEnum.GONE -> Modifier
-                VisibilityEnum.INVISIBLE -> Modifier.alpha(0f)
-                VisibilityEnum.VISIBLE -> Modifier.alpha(1f)
-            }
-        }
-    )
+fun Modifier.invisibleIf(invisible: Boolean) = if (invisible) drawWithContent {  } else this
+
+inline fun Modifier.invisibleIf(crossinline invisible: () -> Boolean) =
+    drawWithContent { if (!invisible()) drawContent() }
