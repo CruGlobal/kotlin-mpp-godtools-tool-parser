b31fa7279b4d5e932486dfa3076a874a71c6b3fa
Daniel Frett <daniel.frett@cru.org>
2025-06-11 12:07:31 -0600

    create an initial pass at RenderImage

---
+++ module/renderer/src/commonMain/kotlin/org/cru/godtools/shared/renderer/content/RenderImage.kt
@@ -0,0 +1,35 @@
+package org.cru.godtools.shared.renderer.content
+
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import coil3.compose.AsyncImage
+import coil3.compose.LocalPlatformContext
+import coil3.request.ImageRequest
+import org.cru.godtools.shared.renderer.content.extensions.alignment
+import org.cru.godtools.shared.renderer.content.extensions.optionalClickable
+import org.cru.godtools.shared.renderer.content.extensions.toPath
+import org.cru.godtools.shared.renderer.content.extensions.width
+import org.cru.godtools.shared.renderer.state.State
+import org.cru.godtools.shared.renderer.util.LocalResourceFileSystem
+import org.cru.godtools.shared.tool.parser.model.Image
+
+@Composable
+internal fun ColumnScope.RenderImage(image: Image, state: State) {
+    val scope = rememberCoroutineScope()
+
+    AsyncImage(
+        model = ImageRequest.Builder(LocalPlatformContext.current)
+            .fileSystem(LocalResourceFileSystem.current)
+            .data(image.resource?.toPath())
+            .build(),
+        contentDescription = null,
+        modifier = Modifier
+            .padding(horizontal = Horizontal_Padding)
+            .width(image.width)
+            .align(image.gravity.alignment)
+            .optionalClickable(image, state, scope)
+    )
+}
