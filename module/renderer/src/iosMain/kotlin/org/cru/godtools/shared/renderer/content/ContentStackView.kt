22de79d5943951401baeaf3c3763e4276c627bf2
Daniel Frett <daniel.frett@cru.org>
2025-06-11 09:08:43 -0600

    support passing in state from iOS for rendering a ContentStack

--- module/renderer/src/iosMain/kotlin/org/cru/godtools/shared/renderer/content/ContentStackView.kt
+++ module/renderer/src/iosMain/kotlin/org/cru/godtools/shared/renderer/content/ContentStackView.kt
@@ -1,10 +1,11 @@
 package org.cru.godtools.shared.renderer.content
 
 import androidx.compose.ui.window.ComposeUIViewController
+import org.cru.godtools.shared.renderer.state.State
 import org.cru.godtools.shared.tool.parser.model.Content
 import platform.UIKit.UIViewController
 
 @Suppress("ktlint:standard:function-naming")
-fun ContentStackView(content: List<Content>): UIViewController = ComposeUIViewController {
-    RenderContentStack(content)
+fun ContentStackView(content: List<Content>, state: State): UIViewController = ComposeUIViewController {
+    RenderContentStack(content, state = state)
 }
