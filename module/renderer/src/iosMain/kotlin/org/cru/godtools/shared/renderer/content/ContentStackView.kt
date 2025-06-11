c33cc0cfde831734b5210393f7c5711f0fd2fc02
Daniel Frett <daniel.frett@cru.org>
2025-06-11 10:55:58 -0600

    make the iOS ViewController transparent so the background shows through

--- module/renderer/src/iosMain/kotlin/org/cru/godtools/shared/renderer/content/ContentStackView.kt
+++ module/renderer/src/iosMain/kotlin/org/cru/godtools/shared/renderer/content/ContentStackView.kt
@@ -3,9 +3,12 @@ package org.cru.godtools.shared.renderer.content
 import androidx.compose.ui.window.ComposeUIViewController
 import org.cru.godtools.shared.renderer.state.State
 import org.cru.godtools.shared.tool.parser.model.Content
+import platform.UIKit.UIColor
 import platform.UIKit.UIViewController
 
 @Suppress("ktlint:standard:function-naming")
 fun ContentStackView(content: List<Content>, state: State): UIViewController = ComposeUIViewController {
     RenderContentStack(content, state = state)
+}.apply {
+    view.backgroundColor = UIColor(white = 0.0, alpha = 0.0)
 }
