201d78e6471e75314cd600addc627b81dc7e0a3c
Daniel Frett <daniel.frett@cru.org>
2025-07-01 11:07:17 -0600

    handle submitting followup forms when processing a click

--- module/renderer/src/commonTest/kotlin/org/cru/godtools/shared/renderer/content/extensions/ClickableStateTest.kt
+++ module/renderer/src/commonTest/kotlin/org/cru/godtools/shared/renderer/content/extensions/ClickableStateTest.kt
@@ -15,7 +15,9 @@ import org.cru.godtools.shared.renderer.state.State
 import org.cru.godtools.shared.tool.parser.model.AnalyticsEvent
 import org.cru.godtools.shared.tool.parser.model.Clickable
 import org.cru.godtools.shared.tool.parser.model.EventId
+import org.cru.godtools.shared.tool.parser.model.Form
 import org.cru.godtools.shared.tool.parser.model.HasAnalyticsEvents
+import org.cru.godtools.shared.tool.parser.model.Input
 
 @RunOnAndroidWith(AndroidJUnit4::class)
 class ClickableStateTest {
@@ -23,7 +25,12 @@ class ClickableStateTest {
     private val event1 = EventId(name = "event1")
     private val event2 = EventId(name = "event2")
 
-    private val state = State()
+    private val form = Form {
+        listOf(
+            Input(it, name = "name", isRequired = true)
+        )
+    }
+    private val state = State().apply { updateFormFieldValue("name", "value") }
 
     private val testScope = TestScope()
 
@@ -77,9 +84,50 @@ class ClickableStateTest {
     }
 
     @Test
+    fun `handleClickable - Submit Form`() = testScope.runTest {
+        val model = object : Clickable {
+            override val parent = form
+            override val events = listOf(EventId.FOLLOWUP)
+            override val url = null
+        }
+
+        state.events.filterIsInstance<State.Event.SubmitForm>().test {
+            model.handleClickable(state, this)
+            assertEquals(State.Event.SubmitForm(mapOf("name" to "value")), awaitItem())
+        }
+    }
+
+    @Test
+    fun `handleClickable - Submit Form - Validation Failure`() = testScope.runTest {
+        state.updateFormFieldValue("name", "")
+        val model = object : Clickable, HasAnalyticsEvents {
+            override val parent get() = form
+            override val events = listOf(event1, EventId.FOLLOWUP, event2)
+            override val url = TestConstants.TEST_URL
+            override fun getAnalyticsEvents(type: AnalyticsEvent.Trigger) = when (type) {
+                AnalyticsEvent.Trigger.CLICKED -> listOf(analyticsEvent)
+                else -> emptyList()
+            }
+        }
+
+        turbineScope {
+            val contentEvents = state.contentEvents.testIn(this)
+            val events = state.events.testIn(this)
+
+            model.handleClickable(state, this@runTest)
+            contentEvents.expectNoEvents()
+            events.expectNoEvents()
+
+            contentEvents.cancel()
+            events.cancel()
+        }
+    }
+
+    @Test
     fun `handleClickable - Trigger Everything`() = testScope.runTest {
         val model = object : Clickable, HasAnalyticsEvents {
-            override val events = listOf(event1, event2)
+            override val parent get() = form
+            override val events = listOf(event1, EventId.FOLLOWUP, event2)
             override val url = TestConstants.TEST_URL
             override fun getAnalyticsEvents(type: AnalyticsEvent.Trigger) = when (type) {
                 AnalyticsEvent.Trigger.CLICKED -> listOf(analyticsEvent)
@@ -90,16 +138,19 @@ class ClickableStateTest {
         turbineScope {
             val analyticsEvents = state.events.filterIsInstance<State.Event.AnalyticsEventTriggered>().testIn(this)
             val contentEvents = state.contentEvents.testIn(this)
+            val submitFormEvents = state.events.filterIsInstance<State.Event.SubmitForm>().testIn(this)
             val urlEvents = state.events.filterIsInstance<State.Event.OpenUrl>().testIn(this)
 
             model.handleClickable(state, this@runTest)
             assertEquals(analyticsEvent, analyticsEvents.awaitItem().event)
             assertEquals(event1, contentEvents.awaitItem())
             assertEquals(event2, contentEvents.awaitItem())
+            assertEquals(State.Event.SubmitForm(mapOf("name" to "value")), submitFormEvents.awaitItem())
             assertEquals(TestConstants.TEST_URL, urlEvents.awaitItem().url)
 
             analyticsEvents.cancel()
             contentEvents.cancel()
+            submitFormEvents.cancel()
             urlEvents.cancel()
         }
     }
