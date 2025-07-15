b1a6e070923609034a1d6f7de6c528ee19eb8adf
Daniel Frett <daniel.frett@cru.org>
2025-07-15 11:24:38 -0600

    add some padding to buttons when displaying larger text

--- module/renderer/src/commonMain/kotlin/org/cru/godtools/shared/renderer/content/RenderButton.kt
+++ module/renderer/src/commonMain/kotlin/org/cru/godtools/shared/renderer/content/RenderButton.kt
@@ -5,7 +5,9 @@ import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.LocalMinimumInteractiveComponentSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
@@ -20,6 +22,8 @@ import org.cru.godtools.shared.renderer.content.extensions.width
 import org.cru.godtools.shared.renderer.state.State
 import org.cru.godtools.shared.tool.parser.model.Button
 
+private val Button_Vertical_Padding = 4.dp
+
 @Composable
 internal fun ColumnScope.RenderButton(button: Button, state: State) {
     val scope = rememberCoroutineScope()
@@ -28,31 +32,38 @@ internal fun ColumnScope.RenderButton(button: Button, state: State) {
         button.isInvisibleFlow(state)
     }.collectAsState(button.isInvisible(state))
 
-    Button(
-        onClick = { button.handleClickable(state, scope) },
-        enabled = !invisible,
-        colors = when (button.style) {
-            Button.Style.OUTLINED -> ButtonDefaults.outlinedButtonColors(
-                containerColor = button.backgroundColor.toComposeColor()
-            )
-            Button.Style.CONTAINED, Button.Style.UNKNOWN -> ButtonDefaults.buttonColors(
-                containerColor = button.buttonColor.toComposeColor()
-            )
-        },
-        elevation = when (button.style) {
-            Button.Style.OUTLINED -> null
-            Button.Style.CONTAINED, Button.Style.UNKNOWN -> ButtonDefaults.buttonElevation()
-        },
-        border = when (button.style) {
-            Button.Style.OUTLINED -> BorderStroke(1.dp, button.buttonColor.toComposeColor())
-            Button.Style.CONTAINED, Button.Style.UNKNOWN -> null
-        },
-        modifier = Modifier
-            .visibility(button, state)
-            .padding(horizontal = Horizontal_Padding)
-            .width(button.width)
-            .align(button.gravity.alignment)
+    val minSize = LocalMinimumInteractiveComponentSize.current
+    CompositionLocalProvider(
+        // We decrease the minimum size to account for the additional vertical padding
+        // we are adding before the minimum size modifier
+        LocalMinimumInteractiveComponentSize provides minSize - (Button_Vertical_Padding * 2)
     ) {
-        RenderTextNode(button.text)
+        Button(
+            onClick = { button.handleClickable(state, scope) },
+            enabled = !invisible,
+            colors = when (button.style) {
+                Button.Style.OUTLINED -> ButtonDefaults.outlinedButtonColors(
+                    containerColor = button.backgroundColor.toComposeColor()
+                )
+                Button.Style.CONTAINED, Button.Style.UNKNOWN -> ButtonDefaults.buttonColors(
+                    containerColor = button.buttonColor.toComposeColor()
+                )
+            },
+            elevation = when (button.style) {
+                Button.Style.OUTLINED -> null
+                Button.Style.CONTAINED, Button.Style.UNKNOWN -> ButtonDefaults.buttonElevation()
+            },
+            border = when (button.style) {
+                Button.Style.OUTLINED -> BorderStroke(1.dp, button.buttonColor.toComposeColor())
+                Button.Style.CONTAINED, Button.Style.UNKNOWN -> null
+            },
+            modifier = Modifier
+                .visibility(button, state)
+                .padding(horizontal = Horizontal_Padding, vertical = Button_Vertical_Padding)
+                .width(button.width)
+                .align(button.gravity.alignment)
+        ) {
+            RenderTextNode(button.text)
+        }
     }
 }
