be0d0c25a0f208f4a88e6bb6907e12d2fe0e82ba
Levi Eggert <levi.eggert@cru.org>
2025-07-15 14:42:38 -0400

    Add render content card

--- module/renderer/src/commonMain/kotlin/org/cru/godtools/shared/renderer/content/RenderContent.kt
+++ module/renderer/src/commonMain/kotlin/org/cru/godtools/shared/renderer/content/RenderContent.kt
@@ -10,6 +10,7 @@ import androidx.compose.ui.graphics.Color
 import org.cru.godtools.shared.renderer.state.State
 import org.cru.godtools.shared.tool.parser.model.Animation
 import org.cru.godtools.shared.tool.parser.model.Button
+import org.cru.godtools.shared.tool.parser.model.Card
 import org.cru.godtools.shared.tool.parser.model.Content
 import org.cru.godtools.shared.tool.parser.model.Flow
 import org.cru.godtools.shared.tool.parser.model.Form
@@ -34,6 +35,7 @@ internal fun ColumnScope.RenderContent(content: Content, state: State) {
     when (content) {
         is Animation -> RenderAnimation(content, state)
         is Button -> RenderButton(content, state)
+        is Card -> RenderContentCard(content, state)
         is Flow -> RenderFlow(content, state)
         is Form -> RenderForm(content, state)
         is Image -> RenderImage(content, state)
