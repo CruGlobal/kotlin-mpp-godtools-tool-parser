583043bc99392725483cc8601a342ad7846bcdb1
Daniel Frett <daniel.frett@cru.org>
2025-07-29 11:21:13 -0600

    use entries instead of values()

--- module/parser/src/commonTest/kotlin/org/cru/godtools/shared/tool/parser/model/TestConstants.kt
+++ module/parser/src/commonTest/kotlin/org/cru/godtools/shared/tool/parser/model/TestConstants.kt
@@ -2,6 +2,6 @@ package org.cru.godtools.shared.tool.parser.model
 
 import org.cru.godtools.shared.common.model.Uri
 
-internal val TEST_GRAVITY = Gravity(Gravity.Horizontal.values().random(), Gravity.Vertical.values().random())
+internal val TEST_GRAVITY = Gravity(Gravity.Horizontal.entries.random(), Gravity.Vertical.entries.random())
 
 expect val TEST_URL: Uri
