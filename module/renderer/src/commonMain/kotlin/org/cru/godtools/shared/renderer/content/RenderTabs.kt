289f38fb59e7cdd516c985c6ed7c9ed28e1b4a56
levieggertcru <59846460+levieggertcru@users.noreply.github.com>
2025-08-21 16:35:28 -0400

    GT-2673 support rendering tabs elements (#903)
    * Expose constructor for Tabs and TabsTab
    
    * Begin rendering tabs
    
    * Change to createTestTab
    
    * Remove RenderTextNode
    
    * Add RenderTabsTest
    
    * Add border
    
    * Add visibility modifier
    
    * Use color from styles
    Add flow with life cycle
    
    * Resolve klint
    
    * Add horizontal padding
    
    * Use by remember
    
    * Use predicate for look up
    
    * Add test tag tabs
    
    * Add content for tab
    
    * Remove unused import
    
    * Change to ColumnScope
    
    * Remove nested spacers
    
    * Link to parent
    
    * Update to RenderContent to send selected tab content or empty
    
    * use null-safe primaryColor & primaryTextColor extension vals to provide fallback behavior
    
    * Remove index =
    
    * Add test for tab is selected when clicked
    
    * Add test for listener
    
    * Use testScope
    
    * Use onAllNodesWithTag when selecting tab
    
    * Remove advanceUntilIdle
    
    * restore the default parent model for tests
    
    * Change to use MaterialTheme shapes small
    
    * Change names to primaryColor and primaryTextColor
    
    * Use assertIsSelected
    
    * runCurrent()
    
    * Use derivedStateOf
    
    * Remove tabs argument label
    
    * Use trailing lambda
    
    ---------
    
    Co-authored-by: Daniel Frett <daniel.frett@cru.org>

---
+++ module/renderer/src/commonMain/kotlin/org/cru/godtools/shared/renderer/content/RenderTabs.kt
@@ -0,0 +1,123 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
+package org.cru.godtools.shared.renderer.content
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SecondaryTabRow
+import androidx.compose.material3.Tab
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.compose.LocalLifecycleOwner
+import androidx.lifecycle.flowWithLifecycle
+import com.github.ajalt.colormath.extensions.android.composecolor.toComposeColor
+import org.cru.godtools.shared.renderer.content.extensions.visibility
+import org.cru.godtools.shared.renderer.state.State
+import org.cru.godtools.shared.tool.parser.model.Tabs
+import org.cru.godtools.shared.tool.parser.model.primaryColor
+import org.cru.godtools.shared.tool.parser.model.primaryTextColor
+import org.cru.godtools.shared.tool.parser.model.stylesParent
+
+internal const val TestTagTabs = "tabs"
+internal const val TestTagTab = "tab"
+
+@Composable
+internal fun ColumnScope.RenderTabs(tabs: Tabs, state: State, modifier: Modifier = Modifier) {
+    val invisible by remember(tabs, state) {
+        tabs.isInvisibleFlow(state)
+    }.collectAsState(tabs.isInvisible(state))
+
+    var selectedIndex by remember { mutableIntStateOf(0) }
+
+    val lifecycleOwner = LocalLifecycleOwner.current
+    val borderColor = tabs.stylesParent.primaryColor.toComposeColor()
+    val borderShape = MaterialTheme.shapes.small
+    val selectedTab by remember(tabs) {
+        derivedStateOf {
+            tabs.tabs.getOrNull(selectedIndex)
+        }
+    }
+
+    LaunchedEffect(tabs, state) {
+        // handle play/stop listeners
+        state.contentEvents
+            .flowWithLifecycle(lifecycleOwner.lifecycle, Lifecycle.State.RESUMED)
+            .collect { event ->
+                tabs.tabs.firstOrNull { event in it.listeners }?.let {
+                    selectedIndex = it.position
+                }
+            }
+    }
+
+    SecondaryTabRow(
+        selectedTabIndex = selectedIndex,
+        modifier = modifier
+            .testTag(tag = TestTagTabs)
+            .visibility(model = tabs, state = state)
+            .padding(horizontal = HorizontalPadding)
+            .border(
+                BorderStroke(width = 2.dp, borderColor),
+                borderShape
+            )
+            // Needed to clip SecondaryTabRow when applying border modifier. ~Levi
+            .clip(borderShape),
+        containerColor = Color.White,
+        contentColor = Color.White,
+        indicator = @Composable {
+        },
+        divider = @Composable {
+        },
+        tabs = @Composable {
+            tabs.tabs.forEachIndexed { index, tab ->
+
+                val isSelected: Boolean = index == selectedIndex
+                val primaryColor = tab.stylesParent.primaryColor.toComposeColor()
+                val primaryTextColor = tab.stylesParent.primaryTextColor.toComposeColor()
+                val backgroundColor = if (isSelected) primaryColor else primaryTextColor
+
+                Tab(
+                    selected = isSelected,
+                    onClick = {
+                        selectedIndex = index
+                    },
+                    modifier = Modifier
+                        .testTag(TestTagTab)
+                        .background(backgroundColor),
+                    text = {
+                        tab.label?.let {
+                            Text(
+                                text = it.text
+                            )
+                        }
+                    },
+                    enabled = !invisible,
+                    selectedContentColor = primaryTextColor,
+                    unselectedContentColor = primaryColor
+                )
+            }
+        }
+    )
+
+    RenderContent(
+        content = selectedTab?.content.orEmpty(),
+        state = state
+    )
+}
---
+++ module/renderer/src/commonTest/kotlin/org/cru/godtools/shared/renderer/content/RenderTabsTest.kt
@@ -0,0 +1,90 @@
+package org.cru.godtools.shared.renderer.content
+
+import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.SemanticsNodeInteractionsProvider
+import androidx.compose.ui.test.assertIsNotSelected
+import androidx.compose.ui.test.assertIsSelected
+import androidx.compose.ui.test.onAllNodesWithTag
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.runComposeUiTest
+import kotlin.test.Test
+import kotlinx.coroutines.test.runCurrent
+import kotlinx.coroutines.test.runTest
+import org.ccci.gto.support.androidx.test.junit.runners.AndroidJUnit4
+import org.ccci.gto.support.androidx.test.junit.runners.RunOnAndroidWith
+import org.cru.godtools.shared.tool.parser.model.EventId
+import org.cru.godtools.shared.tool.parser.model.Tabs
+import org.cru.godtools.shared.tool.parser.model.Text
+
+@RunOnAndroidWith(AndroidJUnit4::class)
+@OptIn(ExperimentalTestApi::class)
+class RenderTabsTest : BaseRenderContentTest() {
+    private companion object {
+        val TAB_1_EVENT_LISTENER = EventId(name = "tab_1_listener")
+        val TAB_2_EVENT_LISTENER = EventId(name = "tab_2_listener")
+    }
+
+    override val testModel = Tabs(invisibleIf = invisibleIf, goneIf = goneIf) {
+        listOf(
+            Tabs.Tab(
+                parent = it,
+                label = Text(text = "1"),
+                listeners = setOf(TAB_1_EVENT_LISTENER)
+            ),
+            Tabs.Tab(
+                parent = it,
+                label = Text(text = "2"),
+                listeners = setOf(TAB_2_EVENT_LISTENER)
+            )
+        )
+    }
+
+    override fun SemanticsNodeInteractionsProvider.onModelNode() = onNodeWithTag(TestTagTabs)
+
+    @Test
+    fun `Action - Second Tab Is Selected When Clicked`() = runComposeUiTest {
+        setContent {
+            RenderContentStack(
+                listOf(testModel),
+                state = state
+            )
+        }
+
+        val firstTab = 0
+        val secondTab = 1
+
+        onAllNodesWithTag(TestTagTab)[firstTab].assertIsSelected()
+        onAllNodesWithTag(TestTagTab)[secondTab].assertIsNotSelected()
+
+        onAllNodesWithTag(TestTagTab)[secondTab].assertExists().performClick()
+
+        onAllNodesWithTag(TestTagTab)[firstTab].assertIsNotSelected()
+        onAllNodesWithTag(TestTagTab)[secondTab].assertIsSelected()
+    }
+
+    @Test
+    fun `Action - Second Tab Is Selected On Content Event`() = runComposeUiTest {
+        setContent {
+            RenderContentStack(
+                listOf(testModel),
+                state = state
+            )
+        }
+
+        val firstTab = 0
+        val secondTab = 1
+
+        onAllNodesWithTag(TestTagTab)[firstTab].assertIsSelected()
+        onAllNodesWithTag(TestTagTab)[secondTab].assertIsNotSelected()
+
+        testScope.runTest {
+            state.triggerContentEvents(listOf(TAB_2_EVENT_LISTENER))
+
+            runCurrent()
+
+            onAllNodesWithTag(TestTagTab)[firstTab].assertIsNotSelected()
+            onAllNodesWithTag(TestTagTab)[secondTab].assertIsSelected()
+        }
+    }
+}
