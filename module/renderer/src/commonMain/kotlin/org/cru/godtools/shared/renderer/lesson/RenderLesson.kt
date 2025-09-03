8e5c8247a5679bec3cddc954177256910944e51e
Daniel Frett <daniel.frett@cru.org>
2025-09-03 15:36:09 -0600

    re-hide hidden pages when the user is no longer viewing them

--- module/renderer/src/commonMain/kotlin/org/cru/godtools/shared/renderer/lesson/RenderLesson.kt
+++ module/renderer/src/commonMain/kotlin/org/cru/godtools/shared/renderer/lesson/RenderLesson.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateSetOf
@@ -13,12 +14,14 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.semantics.SemanticsPropertyKey
 import androidx.compose.ui.semantics.semantics
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.compose.LocalLifecycleOwner
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 import org.ccci.gto.android.common.androidx.lifecycle.ConstrainedStateLifecycleOwner
 import org.cru.godtools.shared.renderer.state.State
@@ -47,6 +50,13 @@ fun RenderLesson(manifest: Manifest, modifier: Modifier = Modifier, state: State
     val pages by remember { derivedStateOf { allPages.filter { it.id in visiblePages || !it.isHidden } } }
 
     val pagerState = rememberPagerState(0) { pages.size }
+
+    // re-hide hidden pages when they are not the current page
+    LaunchedEffect(pagerState) {
+        snapshotFlow { pagerState.settledPage }
+            .map { pages[pagerState.currentPage].id }
+            .collect { pageId -> visiblePages.removeAll { it != pageId } }
+    }
 
     // handle page listeners
     ContentEventListener(state) { eventId ->
