a9f099b337fe10755e0057c9cfd54e96f5c8982d
Daniel Frett <daniel.frett@cru.org>
2025-09-09 10:40:24 -0400

    create a LessonScreen to track the current render state for Lessons

---
+++ module/renderer/src/commonMain/kotlin/org/cru/godtools/shared/renderer/lesson/LessonScreen.kt
@@ -0,0 +1,26 @@
+package org.cru.godtools.shared.renderer.lesson
+
+import com.slack.circuit.runtime.CircuitUiEvent
+import com.slack.circuit.runtime.CircuitUiState
+import org.cru.godtools.shared.renderer.state.State
+import org.cru.godtools.shared.tool.parser.model.Manifest
+
+data object LessonScreen {
+    sealed interface UiState : CircuitUiState {
+        val manifest: Manifest? get() = null
+        val eventSink: (UiEvent) -> Unit
+
+        data class Loaded(
+            val state: State,
+            override val manifest: Manifest,
+            override val eventSink: (UiEvent) -> Unit = {}
+        ) : UiState
+        data class Loading(val progress: Float, override val eventSink: (UiEvent) -> Unit = {}) : UiState
+        data class Missing(override val eventSink: (UiEvent) -> Unit = {}) : UiState
+        data class Offline(override val eventSink: (UiEvent) -> Unit = {}) : UiState
+    }
+
+    sealed interface UiEvent : CircuitUiEvent {
+        data object CloseLesson : UiEvent
+    }
+}
