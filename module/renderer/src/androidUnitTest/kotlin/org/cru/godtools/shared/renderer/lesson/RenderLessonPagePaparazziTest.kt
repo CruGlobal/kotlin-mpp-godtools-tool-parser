e828f8fcadd7892491dc955f31a3e34393c43ae2
Daniel Frett <daniel.frett@cru.org>
2025-09-11 15:11:42 -0400

    support contentInsets for RenderLessonPage

--- module/renderer/src/androidUnitTest/kotlin/org/cru/godtools/shared/renderer/lesson/RenderLessonPagePaparazziTest.kt
+++ module/renderer/src/androidUnitTest/kotlin/org/cru/godtools/shared/renderer/lesson/RenderLessonPagePaparazziTest.kt
@@ -1,8 +1,18 @@
 package org.cru.godtools.shared.renderer.lesson
 
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.github.ajalt.colormath.model.RGB
 import kotlin.test.Test
 import org.cru.godtools.shared.renderer.BasePaparazziTest
+import org.cru.godtools.shared.tool.parser.model.Image
+import org.cru.godtools.shared.tool.parser.model.ImageScaleType
 import org.cru.godtools.shared.tool.parser.model.Manifest
 import org.cru.godtools.shared.tool.parser.model.Spacer
 import org.cru.godtools.shared.tool.parser.model.Text
@@ -71,6 +81,43 @@ class RenderLessonPagePaparazziTest : BasePaparazziTest() {
         manifest.pages.forEachIndexed { i, page ->
             contentSnapshot("page_${i + 1}") {
                 RenderLessonPage(page as LessonPage)
+            }
+        }
+    }
+
+    @Test
+    fun `RenderLessonPage() - Insets - Background`() {
+        val page = LessonPage(
+            manifest,
+            backgroundColor = RGB(0, 1, 1),
+            backgroundImage = "black_panther",
+            backgroundImageScaleType = ImageScaleType.FILL,
+            controlColor = RGB(1, 0, 1),
+        ) {
+            listOf(
+                Text(it, text = "Top Content", textColor = RGB(0, 1, 0)),
+                Spacer(),
+                Image(it, resource = "bruce"),
+                Spacer(),
+                Text(it, text = "Bottom Content", textColor = RGB(0, 1, 0))
+            )
+        }
+
+        contentSnapshot {
+            val insets = PaddingValues(
+                start = 20.dp,
+                top = 50.dp,
+                end = 40.dp,
+                bottom = 30.dp
+            )
+            Box {
+                RenderLessonPage(page, contentInsets = insets, modifier = Modifier.fillMaxSize())
+                Box(
+                    Modifier
+                        .fillMaxSize()
+                        .padding(insets)
+                        .border(1.dp, Color.Red)
+                )
             }
         }
     }
