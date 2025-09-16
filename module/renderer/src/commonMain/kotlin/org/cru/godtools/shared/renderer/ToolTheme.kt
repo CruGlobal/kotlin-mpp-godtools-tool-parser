9382abfbcd279e0654f2a26887599126b440eb87
Daniel Frett <daniel.frett@cru.org>
2025-09-16 10:17:49 -0400

    create a ToolTheme to capture several theme attributes

---
+++ module/renderer/src/commonMain/kotlin/org/cru/godtools/shared/renderer/ToolTheme.kt
@@ -0,0 +1,16 @@
+package org.cru.godtools.shared.renderer
+
+import androidx.compose.material3.CardDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.dp
+
+internal object ToolTheme {
+    @Composable
+    fun cardElevation() = CardDefaults.elevatedCardElevation(
+        defaultElevation = 3.dp,
+        focusedElevation = 3.dp,
+        hoveredElevation = 4.dp,
+        pressedElevation = 1.dp,
+        disabledElevation = 3.dp,
+    )
+}
--- module/renderer/src/commonMain/kotlin/org/cru/godtools/shared/renderer/content/RenderAccordion.kt
+++ module/renderer/src/commonMain/kotlin/org/cru/godtools/shared/renderer/content/RenderAccordion.kt
@@ -31,6 +31,7 @@ import androidx.compose.ui.semantics.selected
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.unit.dp
 import com.github.ajalt.colormath.extensions.android.composecolor.toComposeColor
+import org.cru.godtools.shared.renderer.ToolTheme
 import org.cru.godtools.shared.renderer.content.extensions.visibility
 import org.cru.godtools.shared.renderer.generated.resources.Res
 import org.cru.godtools.shared.renderer.generated.resources.tool_renderer_accordion_section_action_collapse
@@ -94,12 +95,10 @@ private fun RenderAccordionSection(
     val cardColor = section.stylesParent.cardBackgroundColor.toComposeColor()
 
     ElevatedCard(
-        elevation = CardDefaults.elevatedCardElevation(
-            defaultElevation = 4.dp
-        ),
         colors = CardDefaults.cardColors(
             containerColor = cardColor
         ),
+        elevation = ToolTheme.cardElevation(),
         modifier = modifier
             .testTag(TestTagAccordionSection)
             .semantics { selected = isExpanded }
