2193723af52a3d6617723a7f34d2495f35498456
Daniel Frett <daniel.frett@cru.org>
2025-10-03 09:25:59 -0600

    move resolve(ExpressionContext) method to the EventId object

--- module/parser/src/commonMain/kotlin/org/cru/godtools/shared/tool/parser/model/EventId.kt
+++ module/parser/src/commonMain/kotlin/org/cru/godtools/shared/tool/parser/model/EventId.kt
@@ -4,6 +4,7 @@ import androidx.annotation.VisibleForTesting
 import kotlin.experimental.ExperimentalObjCRefinement
 import kotlin.js.ExperimentalJsExport
 import kotlin.js.JsExport
+import org.cru.godtools.shared.tool.parser.expressions.ExpressionContext
 
 @JsExport
 @OptIn(ExperimentalJsExport::class, ExperimentalObjCRefinement::class)
@@ -11,9 +12,14 @@ class EventId(val namespace: String? = null, val name: String) {
     companion object {
         private const val NAMESPACE_FOLLOWUP = "followup"
         @VisibleForTesting
-        const val NAMESPACE_STATE = "state"
+        internal const val NAMESPACE_STATE = "state"
 
         val FOLLOWUP = EventId(NAMESPACE_FOLLOWUP, "send")
+    }
+
+    fun resolve(ctx: ExpressionContext) = when (namespace) {
+        NAMESPACE_STATE -> ctx.getVar(name).map { EventId(name = it) }
+        else -> listOf(this)
     }
 
     override fun equals(other: Any?) = other is EventId &&
