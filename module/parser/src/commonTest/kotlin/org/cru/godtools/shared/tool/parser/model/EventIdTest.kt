f8c72b9884589b2c739c3013bd074ca4d95b3db0
Daniel Frett <daniel.frett@cru.org>
2025-10-03 09:25:59 -0600

    move resolve(ExpressionContext) method to the EventId object

--- module/parser/src/commonTest/kotlin/org/cru/godtools/shared/tool/parser/model/EventIdTest.kt
+++ module/parser/src/commonTest/kotlin/org/cru/godtools/shared/tool/parser/model/EventIdTest.kt
@@ -4,11 +4,38 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertFalse
 import kotlin.test.assertNotEquals
+import kotlin.test.assertTrue
+import org.cru.godtools.shared.tool.parser.expressions.SimpleExpressionContext
 
 class EventIdTest {
     companion object {
         private val ID1 = EventId("followup", "seND")
     }
+
+    // region resolve(ExpressionContext)
+    @Test
+    fun testResolve() {
+        val ctx = SimpleExpressionContext()
+        ctx.setVar("selectorState", listOf("a", "b"))
+        val events = EventId(EventId.NAMESPACE_STATE, "selectorState").resolve(ctx)
+        assertEquals(2, events.size)
+        assertEquals(EventId(name = "a"), events[0])
+        assertEquals(EventId(name = "b"), events[1])
+    }
+
+    @Test
+    fun testResolveUnset() {
+        assertTrue(EventId(EventId.NAMESPACE_STATE, "missing").resolve(SimpleExpressionContext()).isEmpty())
+    }
+
+    @Test
+    fun testResolveDifferentNamespace() {
+        val ctx = SimpleExpressionContext()
+        val event = EventId(name = "event")
+        ctx.setVar("event", listOf("a", "b"))
+        assertEquals(event, event.resolve(ctx).single())
+    }
+    // endregion resolve(ExpressionContext)
 
     @Test
     fun verifyHashCode() {
