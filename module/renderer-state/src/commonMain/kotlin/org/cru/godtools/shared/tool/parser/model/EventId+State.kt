1d47c7456353de1c5f888ef677515e50a02d48db
zachery with an e <45150570+zweatshirt@users.noreply.github.com>
2025-10-06 15:55:40 -0500

    Try to fix inaccessible name and namespace properties issue

--- module/renderer-state/src/commonMain/kotlin/org/cru/godtools/shared/tool/parser/model/EventId+State.kt
+++ module/renderer-state/src/commonMain/kotlin/org/cru/godtools/shared/tool/parser/model/EventId+State.kt
@@ -2,8 +2,12 @@
 
 package org.cru.godtools.shared.tool.parser.model
 
+import kotlin.js.ExperimentalJsExport
+import kotlin.js.JsExport
 import org.cru.godtools.shared.renderer.state.State
 
+@JsExport
+@OptIn(ExperimentalJsExport::class)
 fun EventId.resolve(state: State) = when (namespace) {
     EventId.NAMESPACE_STATE -> state.getVar(name).map { EventId(name = it) }
     else -> listOf(this)
