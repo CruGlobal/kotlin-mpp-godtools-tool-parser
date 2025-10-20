a1a1b854161e901be36dbb084a255e36c22d44b1
levieggertcru <59846460+levieggertcru@users.noreply.github.com>
2025-10-20 12:20:37 +0000

    Package swift updated with checksum 464314027e53ea91aa483a0fedc0fe49b8be69322a244be7d19fafa654ee6c16

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+  name: "GodToolsShared",
+  platforms: [
+    .iOS(.v16),
+  ],
+  products: [
+      .library(name: "GodToolsShared", targets: ["GodToolsShared"])
+  ],
+  targets: [
+      .binaryTarget(
+        name: "GodToolsShared",
+        url: "<link to the uploaded XCFramework ZIP file>",
+        checksum: "464314027e53ea91aa483a0fedc0fe49b8be69322a244be7d19fafa654ee6c16")
+  ]
+)
