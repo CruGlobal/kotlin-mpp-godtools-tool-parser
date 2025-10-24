6c565afec0542862f993bc337f136cd13ff4a75f
Levi Eggert <levi.eggert@cru.org>
2025-10-24 13:12:23 -0400

    Export modules for XCFramework

--- publishing/spm/build.gradle.kts
+++ publishing/spm/build.gradle.kts
@@ -13,12 +13,33 @@ kotlin {
         iosSimulatorArm64(),
     ).forEach {
         it.binaries.framework {
+
+            export(project(":module:analytics"))
+            export(project(":module:interop"))
+            export(project(":module:parser"))
+            export(project(":module:parser-base"))
+            export(project(":module:renderer-state"))
+            export(project(":module:user-activity"))
+
             baseName = xcframeworkName
 
             // Specify CFBundleIdentifier to uniquely identify the framework
             binaryOption("bundleId", "org.cru.${xcframeworkName}")
             xcf.add(this)
             isStatic = false
+        }
+    }
+
+    sourceSets {
+        commonMain {
+            dependencies {
+                api(project(":module:analytics"))
+                api(project(":module:interop"))
+                api(project(":module:parser"))
+                api(project(":module:parser-base"))
+                api(project(":module:renderer-state"))
+                api(project(":module:user-activity"))
+            }
         }
     }
 }
