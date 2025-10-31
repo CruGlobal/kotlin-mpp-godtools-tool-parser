8b68a9d208d8f5e5810acbd6b5458ce3f05ba8ba
Daniel Frett <daniel.frett@cru.org>
2025-10-31 14:09:45 -0600

    add the visibility modifier to forms, paragraphs, and Tab content

--- module/renderer/src/commonMain/kotlin/org/cru/godtools/shared/renderer/content/RenderParagraph.kt
+++ module/renderer/src/commonMain/kotlin/org/cru/godtools/shared/renderer/content/RenderParagraph.kt
@@ -1,19 +1,22 @@
 package org.cru.godtools.shared.renderer.content
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import org.cru.godtools.shared.renderer.content.extensions.visibility
 import org.cru.godtools.shared.renderer.state.State
 import org.cru.godtools.shared.tool.parser.model.Paragraph
 
 private val PARAGRAPH_VERTICAL_PADDING = 10.dp
 
 @Composable
-internal fun ColumnScope.RenderParagraph(paragraph: Paragraph, state: State) {
-    Spacer(Modifier.height(PARAGRAPH_VERTICAL_PADDING))
+internal fun ColumnScope.RenderParagraph(paragraph: Paragraph, state: State) = Column(
+    modifier = Modifier
+        .visibility(paragraph, state)
+        .padding(vertical = PARAGRAPH_VERTICAL_PADDING)
+) {
     RenderContent(paragraph.content, state)
-    Spacer(Modifier.height(PARAGRAPH_VERTICAL_PADDING))
 }
