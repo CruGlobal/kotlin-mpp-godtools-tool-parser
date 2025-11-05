88e9ae672e02acdf92587566df60ec47d1894f15
Daniel Frett <daniel.frett@cru.org>
2025-11-05 16:50:32 -0700

    add section content visibility tests

--- module/renderer/src/commonTest/kotlin/org/cru/godtools/shared/renderer/content/RenderAccordionTest.kt
+++ module/renderer/src/commonTest/kotlin/org/cru/godtools/shared/renderer/content/RenderAccordionTest.kt
@@ -7,6 +7,7 @@ import androidx.compose.ui.test.SemanticsNodeInteractionsProvider
 import androidx.compose.ui.test.assert
 import androidx.compose.ui.test.onAllNodesWithTag
 import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performSemanticsAction
 import androidx.compose.ui.test.runComposeUiTest
@@ -16,6 +17,7 @@ import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.ccci.gto.support.androidx.test.junit.runners.AndroidJUnit4
 import org.ccci.gto.support.androidx.test.junit.runners.RunOnAndroidWith
 import org.cru.godtools.shared.tool.parser.model.Accordion
+import org.cru.godtools.shared.tool.parser.model.Text
 
 @RunOnAndroidWith(AndroidJUnit4::class)
 @OptIn(ExperimentalTestApi::class, ExperimentalCoroutinesApi::class)
@@ -25,16 +27,17 @@ class RenderAccordionTest : BaseRenderContentTest() {
         goneIf = goneIf
     ) {
         listOf(
-            Accordion.Section(it),
-            Accordion.Section(it),
-            Accordion.Section(it)
+            Accordion.Section(it) { listOf(Text(it, "Section 1 Content")) },
+            Accordion.Section(it) { listOf(Text(it, "Section 2 Content")) },
+            Accordion.Section(it) { listOf(Text(it, "Section 3 Content")) },
         )
     }
 
     // region Semantics Nodes
     override fun SemanticsNodeInteractionsProvider.onModelNode() = onNodeWithTag(TestTagAccordion)
-    private fun SemanticsNodeInteractionsProvider.onSectionNode(index: Int) =
-        onAllNodesWithTag(TestTagAccordionSection)[index]
+    private fun SemanticsNodeInteractionsProvider.onSectionNode(section: Int) =
+        onAllNodesWithTag(TestTagAccordionSection)[section]
+
     private val sectionIsExpanded =
         SemanticsMatcher.keyNotDefined(SemanticsActions.Expand)
             .and(SemanticsMatcher.keyIsDefined(SemanticsActions.Collapse))
@@ -42,6 +45,31 @@ class RenderAccordionTest : BaseRenderContentTest() {
         SemanticsMatcher.keyNotDefined(SemanticsActions.Collapse)
             .and(SemanticsMatcher.keyIsDefined(SemanticsActions.Expand))
     // endregion Semantics Nodes
+
+    // region UI - Section - Content
+    @Test
+    fun `UI - Section - Content - Hidden When Collapsed`() = runComposeUiTest {
+        setContent {
+            ProvideTestCompositionLocals {
+                RenderContentStack(listOf(testModel), state = state)
+            }
+        }
+
+        onNodeWithText("Section 1 Content").assertDoesNotExist()
+    }
+
+    @Test
+    fun `UI - Section - Content - Visible When Expanded`() = runComposeUiTest {
+        setContent {
+            ProvideTestCompositionLocals {
+                RenderContentStack(listOf(testModel), state = state)
+            }
+        }
+
+        onSectionNode(0).performSemanticsAction(SemanticsActions.Expand)
+        onNodeWithText("Section 1 Content").assertExists()
+    }
+    // endregion UI - Section - Content
 
     @Test
     fun `Action - Section - Click - Toggles section`() = runComposeUiTest {
