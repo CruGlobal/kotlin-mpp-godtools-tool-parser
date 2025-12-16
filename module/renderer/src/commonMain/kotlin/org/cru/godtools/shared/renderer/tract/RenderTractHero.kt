7e7d2385a5674b20f12fe8595740031356c61841
Daniel Frett <daniel.frett@cru.org>
2025-12-16 10:22:00 -0700

    make sure tract hero images are horizontally aligned correctly

--- module/renderer/src/commonMain/kotlin/org/cru/godtools/shared/renderer/tract/RenderTractHero.kt
+++ module/renderer/src/commonMain/kotlin/org/cru/godtools/shared/renderer/tract/RenderTractHero.kt
@@ -5,10 +5,10 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.LocalTextStyle
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ProvideTextStyle
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -103,7 +103,11 @@ fun RenderTractHero(page: TractPage, modifier: Modifier = Modifier, state: State
                 onPauseOrDispose { visible.forEach { it.cancel() } }
             }
 
-            Column(Modifier.padding(horizontal = 16.dp)) {
+            Column(
+                Modifier
+                    .padding(horizontal = 16.dp)
+                    .fillMaxWidth()
+            ) {
                 hero.heading?.let { heading ->
                     ProvideTextStyle(ToolTheme.TractHeroHeadingTextStyle) {
                         RenderTextNode(
